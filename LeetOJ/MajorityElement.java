a7079038bb1349acfacdf4b70c48e73be5727174
Tony <ntutonycao@gmail.com>
2015-10-29 20:08:07 +0800

    Added one solution

---
+++ LeetOJ/MajorityElement.java
@@ -0,0 +1,58 @@
+
+/*
+ * 
+https://leetcode.com/problems/majority-element/
+
+Given an array of size n, find the majority element. The majority element is the element that appears more than ⌊ n/2 ⌋ times.
+
+You may assume that the array is non-empty and the majority element always exist in the array.
+
+public class Solution {
+    public int majorityElement(int[] nums) {
+        
+    }
+}
+
+ * 
+ */
+import java.util.*;
+
+public class MajorityElement {
+
+	public static void main(String args[]) {
+		int[] myarr = { 2 };
+		System.out.println(majorityElement(myarr));
+	}
+
+	/*
+	 * 
+	 * Accepted:
+	 * 
+	 */
+	public static int majorityElement(int[] nums) {
+
+		int majorN = -1;
+		int maxOcc = -1;
+		int curr = 0;
+		HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+
+		for (int tmp : nums) {
+			if (map.get(tmp) == null) {
+				map.put(tmp, 1);
+				curr = 1;
+			} else {
+				curr = map.get(tmp);
+				map.put(tmp, curr + 1);
+				curr++;
+			}
+			if (maxOcc < curr) {
+				majorN = tmp;
+				maxOcc = curr;
+			}
+		}
+
+		return majorN;
+
+	}
+
+}
