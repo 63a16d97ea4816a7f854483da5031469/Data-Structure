0173e182bb819f5a0cf15774301596b47962ec6a
Tony <ntutonycao@gmail.com>
2015-11-16 11:44:53 +0800

    Added solution

--- LeetOJ/UglyNumberii.java
+++ LeetOJ/UglyNumberii.java
@@ -25,9 +25,74 @@ public class UglyNumberii {
 	
 	public static void main(String args[]){
 		UglyNumberii i=new UglyNumberii();
-		System.out.println(i.nthUglyNumber(459));
+		System.out.println(i.nthUglyNumber4(9));
 	}
+
 	
+	
+	
+	
+//As I reference to other people's code, I need to rewrite it by myself:
+	
+//--------------------------------------------------------------------------------------------------------------------------------------------
+	
+	
+	
+	public int nthUglyNumber4(int n){
+
+		int i2=0,i3=0,i5=0;
+		int[] ugly=new int[n];
+		ugly[0]=1;
+
+		int next_mutiple_of_2=2;
+		int next_mutiple_of_3=3;
+		int next_mutiple_of_5=5;
+		int next_ugly_no=1;
+
+		for(int i=1;i<n;i++){
+
+		next_ugly_no=Math.min(next_mutiple_of_2,Math.min(next_mutiple_of_3,next_mutiple_of_5));
+
+//		System.out.println(next_mutiple_of_2+" "+next_mutiple_of_3+" "+next_mutiple_of_5);
+		
+		ugly[i]=next_ugly_no;
+
+		if(next_ugly_no==next_mutiple_of_2){
+		    i2++;
+		   next_mutiple_of_2=ugly[i2]*2;
+		}
+
+		if(next_ugly_no==next_mutiple_of_3){
+		    i3++;
+		    next_mutiple_of_3=ugly[i3]*3;
+		}
+
+		if(next_ugly_no==next_mutiple_of_5){
+		    i5++;
+		    next_mutiple_of_5=ugly[i5]*5;
+		}
+
+
+		}
+
+		return next_ugly_no;
+
+		}
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+//--------------------------------------------------------------------------------------------------------------------------------------------	
+	
+	
+
 	
 	/*
 	 * Reference to http://www.geeksforgeeks.org/ugly-numbers/
@@ -44,10 +109,53 @@ public class UglyNumberii {
 	 * 4, 5, …) multiply 2, 3, 5. Then we use similar merge method as merge
 	 * sort, to get every ugly number from the three subsequence. Every step we
 	 * choose the smallest one, and move one step after.
+	 * 
+	 * 
+	 * Accepted.
+	 * 
+	 * 
 	 */
+ 
 	
+	public int nthUglyNumber3(int n){
+		int i2=0,i3=0,i5=0;
+		int[] ugly=new int[n];
+		ugly[0]=1;
+		int next_mutiply_of_2=ugly[i2]*2;
+		int next_mutiply_of_3=ugly[i3]*3;
+		int next_mutiply_of_5=ugly[i5]*5;
+		int next_ugly_no=1;
+		for(int i=1;i<n;i++){
+			next_ugly_no=Math.min(next_mutiply_of_5,Math.min(next_mutiply_of_2, next_mutiply_of_3));
+ 
+			ugly[i]=next_ugly_no;
+//			System.out.println(next_mutiply_of_2+" "+next_mutiply_of_3+" "+next_mutiply_of_5);
+			
+			if(next_ugly_no==next_mutiply_of_2){
+				i2++;
+				next_mutiply_of_2=ugly[i2]*2;
+
 	
+			}
+			
+			if(next_ugly_no==next_mutiply_of_3){
+				i3++;
+				next_mutiply_of_3=ugly[i3]*3;
 	
+			}
+			
+			if(next_ugly_no==next_mutiply_of_5){
+				i5++;
+				next_mutiply_of_5=ugly[i5]*5;
+
+			}
+			
+//			System.out.print(next_ugly_no+"  ");
+		}
+		
+		
+		return next_ugly_no;
+	}
 	
 	
 	
