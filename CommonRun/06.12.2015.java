f71d6871eaa1c01bd22456fe0dad049a1a48fc24
Tony <ntutonycao@gmail.com>
2015-12-06 22:10:48 +0800

    modified 06.12.2015 practice

--- CommonRun/06.12.2015.java
+++ CommonRun/06.12.2015.java
@@ -123,19 +123,196 @@ public void loopList(TreeNode head){
 
 ##Fibonacci Number
 
+public void findN(int n){
+	if(n==0||n==1) return 1;
+
+	return findN(n-1)+findN(n-2);
+}
+
+
+public void findFibonacci(int n){
+	if(n==0||n==1) return 1;
+
+	int a=1;
+	int b=1;
+	int sum=a+b;
+
+	for(int i=2;i<=n;i++){
+		sum=a+b;
+		a=b;
+		b=sum;
+	}
+
+	return sum;
+}
+
 ##BFS
 
 ##DFS
 
 ##Judge whether has cycle
 
+class ListNode{
+	int val;
+	ListNode next;
+	ListNode(int x){val=x;}
+}
+
+public boolean isCycle(ListNode head){
+	ListNode fast=head;
+	ListNode slow=head;
+
+	while(fast!=null){
+		if(fast.next!=null){
+			fast=fast.next.next;
+		}else{
+			fast=fast.next;
+		}
+
+		slow=slow.next;
+
+		if(fast==slow) return true;
+	}
+
+return false;
+}
+
 ##find the insection node of two single lists
 
+class ListNode{
+	int val;
+	ListNode next;
+	ListNode(int x){val=x;}
+}
+
+public ListNode findInsection(ListNode head1, ListNode head2){
+
+	ListNode tmp1=head1;
+	ListNode tmp2=head2;
+
+	int len1=0;
+	int len2=0;
+
+	while(tmp1!=null){
+		tmp1=tmp1.next;
+		len1++;
+	}
+
+	while(tmp2!=null){
+		tmp2=tmp2.next;
+		len2++;
+	}
+
+	int diff=0;
+
+	if(len1>len2){
+		diff=len1-len2;
+
+		while(diff>0){
+			diff--;
+			tmp1=tmp1.next;
+		}
+	}
+
+	if(len2>len1){
+		diff=len2-len1;
+
+		while(diff>0){
+			diff--;
+			tmp2=tmp2.next;
+		}
+	}
+
+	while(tmp1!=null&&tmp2!=null){
+
+		tmp1=tmp1.next;
+		tmp2=tmp2.next;
+
+		if(tmp1.val==tmp2.val) return tmp1;
+	}
+return null;
+}
+
 ##find the beginning node of cycle (Linked List)
+
+class ListNode{
+	int val;
+	ListNode next;
+	ListNode(int x){val=x;}
+}
+
+public ListNode findBegin(TreeNode head){
+	ListNode fast=head;
+	ListNode slow=head;
+
+	while(fast!=null){
+		if(fast.next!=null){
+			fast=fast.next.next;
+		}else{
+			fast=fast.next;
+		}
+
+		slow=slow.next;
+
+		if(fast==slow) break;
+	}
+
+	fast=head;
+
+	while(fast!=null&&slow!=null){
+		fast=fast.next;
+		slow=slow.next;
+		if(fast==slow) return fast;
+	}
+
+return null;
+}
+
 
 ##LRU Cache
 
 ##Minimum Depth of Binary Tree
+
+class TreeNode{
+	int val;
+	TreeNode left;
+	TreeNode right;
+	TreeNode(int x){val=x;}
+}
+
+public int finMin(TreeNode root){
+	int depth=0;
+
+	LinkedList<TreeNode> que=new LinkedList<TreeNode>();
+	que.addLast(root);
+	que.addLast(null);
+
+	while(!que.isEmpty()){
+
+		TreeNode firstNode=que.removeFirst();
+
+		if(firstNode==null){
+			depth++;
+			if(!que.isEmpty()){
+				que.addLast(null);
+			}
+		}else{
+			if(firstNode.left==null&&firstNode.right==null){
+				return depth+1;
+			}
+
+			if(firstNode.left!=null){
+				que.addLast(firstNode.left);
+			}
+
+			if(firstNode.right!=null){
+				que.addLast(firstNode.right);
+			}
+		}
+	}
+	return depth;
+}
+
 
 ##Maximum Depth of Binary Tree
 
