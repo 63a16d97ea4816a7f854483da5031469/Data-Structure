4c032ab0d1bb3032662b676c60d5fc207a81b411
Tony <ntutonycao@gmail.com>
2015-12-07 11:52:22 +0800

    Added practice

--- CommonRun/06.12.2015.java
+++ CommonRun/06.12.2015.java
@@ -1,3 +1,6 @@
+/*
+
+
 # Must write within 1 minute
 Remember:    
 
@@ -318,11 +321,54 @@ public int finMin(TreeNode root){
 
 ##Binary Search
 
+public int binarySearch(int[] nums,int key){
+	
+	int low=0;
+	int high=nums.length-1;
+
+	while(low<=high){
+		int middle=(low+high)/2;
+		if(nums[middle]==key) return middle;
+
+		if(nums[middle]>key) {
+			high=middle-1;
+		}
+
+		if(nums[middle]<key){
+			low=middle+1;
+		}
+	}
+return -1;
+}
+
+
 ##Implement Queue using stacks
 
 ##Implement Stack using Queues
 
 ##invert binary tree
+
+public void invertTree(TreeNode root){
+	
+	if(root!=null){
+		if(root.left!=null||root.right!=null){
+			TreeNode tmp=root.left;
+			root.left=root.right;
+			root.right=tmp;
+		}
+
+		if(root.left!=null){
+			invertTree(root.left);
+		}
+
+		if(root.right!=null){
+			invertTree(root.right);
+		}
+
+	}
+
+}
+
 
 ##Swap two variables without using extra space
 
@@ -334,38 +380,234 @@ public int finMin(TreeNode root){
 
 ##Move-zeroes  ===> array operation
 
+public void moveZero(int[] nums){
+	
+	int p=0;
+	while(p<nums.length){
+		if(nums[p]==0){
+			for(int i=p+1;i<nums.length;i++){
+				if(nums[i]!=0){
+					nums[p]=nums[i];
+					nums[i]=0;
+					break;
+				}
+			}
+		}
+		p++;
+	}
+
+}
+
 ##Palindrome Number (回文)
 
 ##Permutations (important)
 
+public void permutations(int[] nums,int i,int n){
+	if(i==n){
+		for(int tmp:nums)
+		System.out.print(tmp+" ");
+
+		System.out.println();
+	}else{
+		for(int j=i;j<nums.length;j++){
+			swap(nums,i,j);
+			permutations(nums,i+1,n);
+			swap(nums,i,j);
+		}
+	}
+}
+
 ## Find path of Binary Tree
+
+class TreeNode{
+	int val;
+	TreeNode left;
+	TreeNode right;
+	TreeNode(int x){val=x;}
+}
+
+List<String> result=new ArrayList<String>();
+List<TreeNode> list=new ArrayList<TreeNode>();
+
+public void findPath(TreeNode root){
+	
+	if(root!=null){
+		list.add(root);
+
+		if(root.left==null&&root.right==null){
+			String str="";
+			for(TreeNode tmp:list)
+			str+=tmp.val;
+
+			result.add(str);
+			list=new ArrayList<TreeNode>();
+		}
+
+		if(root.left!=null){
+			findPath(root.left);
+			list.remove(list.size()-1);
+		}
+
+		if(root.right!=null){
+			findPath(root.right);
+			list.remove(list.size()-1);
+		}
+
+	}
+
+}
+
+
 
 ##ReverseBits
 
+public String reverseBit(int n){
+	StringBuilder str=new StringBuilder(Integer.toBinaryString(n));
+	return str.reverse().toString();
+}
+
+public String reverseInt(int n){
+	String str=Integer.toBinaryString(n);
+	int low=0;
+	int high=nums.length-1;
+	Char[] cstr=str.toCharArray();
+	while(low<high){
+	Char tmp=cstr[low];
+	cstr[low]=cstr[high];
+	cstr[high]=tmp;
+	low++;
+	high--;
+	}
+}
+
+
 ##Reverse Integer
 
+public void reverseInt(int n){
+	int another=0;
+	int tmp=n;
+	while(tmp!=0){
+ 	another=another*10+tmp%10;
+ 	tmp/=10;
+	}
+}
 ##Reverse Linked List
+
+class ListNode{
+	int val;
+	ListNode next;
+	ListNode(int x){val=x;}
+}
+
+public void loopList(ListNode head){
+	
+	ListNode nextNext=head;
+	ListNode prev=null;
+	ListNode curr=head;
+
+	while(curr!=null){
+		nextNext=curr.next;
+		curr.next=prev;
+		prev=curr;
+		curr=nextNext;
+	}
+
+}
+
 
 ##Rotate Array
 
+public void rotateArr(int[] nums){
+	
+	int[] c=nums.clone();
+
+	for(int i=0;i<nums.length;i++){
+		nums[i]=c[(i+k)%nums.length];
+	}
+
+}
+
+
 ##Reverse Array
+
+public void reverseArray(int[] nums){
+	int low=0;
+	int high=nums.length-1;
+
+	while(low<high){
+	int tmp=nums[low];
+	nums[low]=nums[high];
+	nums[high]=tmp;
+
+	low++;
+	high--;
+	}
+}
+
 
 ##Is Same Tree (/same-tree/)
 
 ## Symmetric Tree
 
+public boolean isSymm(TreeNode root){
+	return isSymmSub(root.left,root.right);
+}
+
+public boolean isSymmSub(TreeNode p,TreeNode q){
+	
+	if(p==null&&q==null) return true;
+	if(p==null||q==null) return false;
+
+	if(p!=null&&q!=null){
+		return (p.val==q.val)&&isSymmSub(p.right,q.left)&&isSymmSub(p.left,q.right);
+	}
+
+}
+
+
+
 ## valid-anagram (/valid-anagram/)
 
 ##HashSet iteration
 
+for(Iterator it=set.iterator();it.hasNext();){
+	System.out.println(it.next());
+}
+
 ##HashMap iteration
 
+for(Entry<String,String> entry=map.EntrySet()){
+	System.out.println(entry.getKey()+":"+entry.getValue());
+}
+
+
 ##HashTable iteration
+
+Hashtable table=new Hashtable();
+table.put("dfs","fds");
+
+Enumeration e=table.keys();
+
+while(e.hasMoreElements()){
+	String str=(String)e.nextElement();
+	System.out.println(str+":"+table.get(str));
+}
+
 
 ##Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
 
 
 ##MaximumSubarray
+
+public int findMax(int[] nums){
+	int max_ending_here=nums[0];
+	int max_so_far=nums[0];
+
+	for(int i=1;i<nums.length;i++){
+		max_ending_here=Math.max(max_ending_here,max_ending_here+nums[i]);
+		max_so_far=Math.max(max_ending_here,max_so_far);
+	}
+}
 
 
 ##LinkRightNode
@@ -379,4 +621,37 @@ class Node
     public int val;
     Node(int x){val=x;}
 }
+
+
+public void linkRight(Node root){
+	
+	LinkedList<Node> que=new LinkedList<Node>();
+	que.addLast(root);
+	que.addLast(null);
+	Node linkRight=null;
+
+	while(!que.isEmpty()){
+		Node firstNode=que.removeFirst();
+		if(firstNode==null){
+			linkRight=null;
+			if(!que.isEmpty()){
+				que.addLast(null)''
+			}
+		}else{
+			
+			if(linkRight!=null){
+				linkRight.Right=firstNode;
+			}
+
+			if(firstNode.Children!=null){
+				for(Node tmp:firstNode.Children)
+				que.addLast(tmp);
+			}
+
+			linkRight=firstNode;
+		}
+	}
+}
+
+
 
---
+++ CommonRun/07.12.2015.java
@@ -0,0 +1,115 @@
+
+/*
+
+
+# Must write within 1 minute
+Remember:    
+
+			String --->  str.length();  
+			Array---> arr.length; 
+			List--->int len=list.size();
+			
+			String.valueOf(char[] ch);
+
+##Binary operation / Bit operations
+	 
+
+##PreOrder Traversal
+
+
+##InOrder Traversal
+
+
+##PostOrder Traversal
+
+
+##LevelOrder Traversal
+
+
+##Deepth of binary tree Traversal
+
+
+##Linked List Traversal
+
+##Fibonacci Number
+
+##BFS
+
+##DFS
+
+##Judge whether has cycle
+
+##find the insection node of two single lists
+
+##find the beginning node of cycle (Linked List)
+
+##LRU Cache
+
+##Minimum Depth of Binary Tree
+
+##Maximum Depth of Binary Tree
+
+##Binary Search
+
+##Implement Queue using stacks
+
+##Implement Stack using Queues
+
+##invert binary tree
+
+##Swap two variables without using extra space
+
+##Merge two sorted arrays ===> array operation
+
+##Merge two sorted lists
+
+##O(1) time complexity to get minimum value of stack
+
+##Move-zeroes  ===> array operation
+
+##Palindrome Number (回文)
+
+##Permutations (important)
+
+## Find path of Binary Tree
+
+##ReverseBits
+
+##Reverse Integer
+
+##Reverse Linked List
+
+##Rotate Array
+
+##Reverse Array
+
+##Is Same Tree (/same-tree/)
+
+## Symmetric Tree
+
+## valid-anagram (/valid-anagram/)
+
+##HashSet iteration
+
+##HashMap iteration
+
+##HashTable iteration
+
+##Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+
+##MaximumSubarray
+
+
+##LinkRightNode
+
+Link all the same level node from left to right by using "Right" field.
+
+class Node
+{
+    public Node[] Children;
+    public Node Right;
+    public int val;
+    Node(int x){val=x;}
+}
+
