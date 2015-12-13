9626fdec490b597de014ad00efe7310507a876eb
Tony <ntutonycao@gmail.com>
2015-12-13 23:04:33 +0800

    Added 13.12.2015 practice.

---
+++ CommonRun/13.12.2015.java
@@ -0,0 +1,123 @@
+# Must write within 1 minute
+Remember:    
+
+			String --->  str.length();  
+			Array---> arr.length; 
+			List--->int len=list.size();
+			
+			String.valueOf(char[] ch);
+			
+			
+##Input n,m    Pick up some numbers from 1,2,3....n, to fulfill the sum of them is equal to m. --can repeat pick up numbers			
+			
+
+
+##Input n,m    Pick up some numbers from 1,2,3....n, to fulfill the sum of them is equal to m. (can repeat)
+
+
+##MaximumSubarray
+
+
+##LinkRightNode
+
+Link all the same level node from left to right by using "Right" field.
+
+class Node
+{
+    public Node[] Children;
+    public Node Right;
+    public int val;
+    Node(int x){val=x;}
+}
+
+##Input n,m    Pick up some numbers from 1,2,3....n, to fulfill the sum of them is equal to m. (0/1 bag)
+
+
+
+##Binary operation / Bit operations  --- The sum of two binary numbers
+	 
+
+##PreOrder Traversal
+
+
+##InOrder Traversal
+
+
+##PostOrder Traversal
+
+
+##LevelOrder Traversal
+
+
+##Deepth of binary tree Traversal
+
+
+##Linked List Traversal
+
+##Fibonacci Number
+
+##BFS
+
+##DFS
+
+##Judge whether has cycle
+
+##find the insection node of two single lists
+
+##find the beginning node of cycle (Linked List)
+
+##LRU Cache
+
+##Minimum Depth of Binary Tree
+
+##Maximum Depth of Binary Tree
+
+##Binary Search
+
+##Implement Queue using stacks
+
+##Implement Stack using Queues
+
+##invert binary tree
+
+##Swap two variables without using extra space
+
+##Merge two sorted arrays ===> array operation
+
+##Merge two sorted lists
+
+##O(1) time complexity to get minimum value of stack
+
+##Move-zeroes  ===> array operation
+
+##Palindrome Number (回文)
+
+##Permutations (important)
+
+## Find path of Binary Tree
+
+##ReverseBits
+
+##Reverse Integer
+
+##Reverse Linked List
+
+##Rotate Array
+
+##Reverse Array
+
+##Is Same Tree (/same-tree/)
+
+## Symmetric Tree
+
+## valid-anagram (/valid-anagram/)
+
+##HashSet iteration
+
+##HashMap iteration
+
+##HashTable iteration
+
+##Returns the index of the first occurrence of needle in haystack, or -1 if needle is not part of haystack.
+
+
