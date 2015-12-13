f790e42f201746e101f118a3a5b4b3836b334811
Tony <ntutonycao@gmail.com>
2015-12-13 23:34:31 +0800

    Modified the content.

--- CommonRun/13.12.2015-2.java
+++ CommonRun/13.12.2015-2.java
@@ -101,23 +101,79 @@ public void linkRight(Node root){
 	que.addLast(root);
 	que.addLast(null);
 
-	Node listRightNode=null;
+	Node linkRightNode=null;
 
 	while(!que.isEmpty()){
 		Node firstNode=que.removeFirst();
+			
+		if(firstNode==null){
+			linkRightNode=null;
 
+			if(!que.isEmpty()){
+				que.addLast(null);
+			}
+		}else{
+			if(linkRightNode!=null){
+			linkRightNode.Right=firstNode;
+			}
+
+			if(firstNode.Children!=null){
+				for(Node tmp:firstNode.Children){
+					que.addLast(tmp);
+				}
+			}
+
+			linkRightNode=firstNode;
+		}
 		
 	}
 
 }
-
-
-
-##Input n,m    Pick up some numbers from 1,2,3....n, to fulfill the sum of them is equal to m. (0/1 bag)
+ 
 
 
 
 ##Binary operation / Bit operations  --- The sum of two binary numbers
+
+	public String binarySum(String a,String b){
+		if(a==null || a.length()==0) return b;
+		if(b==null || b.length()==0) return a;
+
+		int currA=a.length()-1;
+		int currB=b.length()-1;
+		int flag=0;
+
+		StringBuilder sb=new StringBuilder();
+
+		while(currA>=0||currB>=0){
+			int va=0;
+			int vb=0;
+
+			if(currA>=0){
+				va=a.charAt(currA)==0?0:1;
+				currA--;
+			}
+
+			if(currB>=0){
+				vb=b.charAt(currB)==0?0:1;
+				currB--;
+			}
+
+			int sum=va+vb+flag;
+			if(sum>=2){
+				sb.append(sum-2);
+				flag=1;
+			}else{
+				sb.append(sum);
+			}
+		}
+
+		if(flag==1){
+			sb.append("1");
+		}
+
+		return sb.reverse().toString();
+	}
 	 
 
 ##PreOrder Traversal
@@ -128,16 +184,139 @@ public void linkRight(Node root){
 
 ##PostOrder Traversal
 
+	class TreeNode{
+	int val;
+	TreeNode left;
+	TreeNode right;
+	TreeNode(int x){
+	val=x;
+	}
+	}
+
+
+	public void postOrder(TreeNode root){
+		
+		if(root.left!=null){
+			postOrder(root.left);
+		}
+
+		if(root.right!=null){
+			postOrder(root.right);
+		}
+
+		System.out.println(root.val);
+	}
+
 
 ##LevelOrder Traversal
+
+class TreeNode{
+	int val;
+	TreeNode left;
+	TreeNode right;
+	TreeNode(int x){val=x;}
+}
+
+	public void levelOrder(TreeNode root){
+		LinkedList<TreeNode> que=new LinkedList<TreeNode>();
+
+		que.addLast(root);
+
+		while(!que.isEmpty()){
+			TreeNode firstNode=que.removeFirst();
+
+			if(firstNode==null){
+				
+			}else{
+				System.out.println(firstNode.val);
+
+				if(firstNode.left!=null){
+					que.addLast(firstNode.left);
+				}
+
+				if(firstNode.right!=null){
+					que.addLast(firstNode.right);
+				}
+			}
+		}
+	}
 
 
 ##Deepth of binary tree Traversal
 
 
+
+	public int findDepth(TreeNode root){
+
+
+		LinkedList<TreeNode> que=new LinkedList<TreeNode>();
+		int depth=0;
+		
+		que.addLast(root);
+		que.addLast(null);
+
+		while(!que.isEmpty()){
+	
+			TreeNode firstNode=que.removeFirst();
+
+			if(firstNode==null){
+					depth++;
+					if(!que.isEmpty()){
+						que.addLast(null);
+					}
+			}else{
+				
+				if(firstNode.left!=null){
+					que.addLast(firstNode.left);
+				}
+
+				if(firstNode.right!=null){
+					que.addLast(firstNode.right);
+				}
+
+			}
+
+		}
+		return depth;
+	}
+
+
 ##Linked List Traversal
 
+class ListNode{
+	int val;
+	ListNode next;
+	ListNode(int x){val=x;}
+}
+
+public void loopList(ListNode head){
+	ListNode tmp=head;
+	while(tmp!=null){
+	System.out.println(tmp.val);
+	tmp=tmp.next;
+	}
+}
+
 ##Fibonacci Number
+
+public int findN(int n){
+	if(n==0||n==1) return 1;
+	return findN(n-1)+findN(n-2);
+}
+
+public int findFibonacci(int n){
+	if(n==0||n==1) return 1;
+	int a=1;
+	int b=1;
+	int sum=a+b;
+
+	for(int i=2;i<=n;i++){
+		sum=a+b;
+		a=b;
+		b=sum;
+	}
+	return sum;
+}
 
 ##BFS
 
@@ -145,23 +324,213 @@ public void linkRight(Node root){
 
 ##Judge whether has cycle
 
+	public boolean isCycle(ListNode head){
+		ListNode fast=head;
+		ListNode slow=head;
+
+		while(fast!=null){
+			if(fast.next!=null){
+				fast=fast.next.next;
+			}else{
+				fast=fast.next;
+			}
+			slow=slow.next;
+
+			if(fast==slow) return true;
+		}
+		return false;
+	}
+
 ##find the insection node of two single lists
 
+class ListNode{
+	int val;
+	ListNode next;
+	ListNode(int x){val=x;}
+}
+
+public ListNode findInsection(ListNode head1,ListNode head2){
+	ListNode tmp1=head1;
+	ListNode tmp2=head2;
+
+	int len1=0;
+	int len2=0;
+
+	while(tmp1!=null){
+		len1++;
+		tmp1=tmp1.next;
+	}
+
+	while(tmp2!=null){
+		len2++;
+		tmp2=tmp2.next;
+	}
+
+	tmp1=head1;
+	tmp2=head2;
+
+	int diff=0;
+
+	if(len1>len2){
+		diff=len1-len2;
+
+		while(diff>0){
+			diff--;
+			tmp1=tmp1.next;
+		}
+	}
+
+	if(len2>len1){
+		diff=len2-len1;
+
+		while(diff>0){
+			diff--;
+			tmp2=tmp2.next;
+		}
+	}
+
+	while(tmp1!=null&&tmp2!=null){
+	
+	tmp1=tmp1.next;
+	tmp2=tmp2.next;
+	if(tmp1.val==tmp2.val) return tmp1;
+	}
+
+	return null;
+}
+
 ##find the beginning node of cycle (Linked List)
+
+class ListNode{
+	int val;
+	ListNode next;
+	ListNode(int x){val=x;}
+}
+
+public ListNode findBegin(ListNode head){
+	ListNode fast=head;
+	ListNode slow=head;
+
+	while(fast!=null){
+			if(fast.next!=null){
+				fast=fast.next.next;
+			}else{
+				fast=fast.next;
+			}
+
+			slow=slow.next;
+
+			if(fast==slow) break;
+	}
+
+	fast=head;
+
+	while(fast!=null&&slow!=null){
+		fast=fast.next;
+		slow=slow.next;
+		if(fast==slow) return fast;
+	}
+	return null;
+}
 
 ##LRU Cache
 
 ##Minimum Depth of Binary Tree
 
+class ListNode{
+	int val;
+	ListNode next;
+	ListNode(int x){val=x;}
+}
+
+public int findDepth(TreeNode root){
+	int depth=0;
+
+	LinkedList<TreeNode> que=new LinkedList<TreeNode>();
+
+	que.addLast(root);
+	que.addLast(null);
+
+	while(!que.isEmpty()){
+		TreeNode firstNode=que.removeFirst();
+
+		if(firstNode==null){
+			depth++;
+			if(!que.isEmpty()){
+				que.addLast(null);
+			}		
+		}else{
+			
+			if(firstNode.left==null&&firstNode.right==null){
+				return depth+1;
+			}
+
+			if(firstNode.left!=null){
+				que.addLast(firstNode.left);
+			}
+
+			if(firstNode.right!=null){
+				que.addLast(firstNode.right);
+			}
+
+		}
+	}
+	return depth;
+}
+
 ##Maximum Depth of Binary Tree
 
 ##Binary Search
+
+	public int binarySearch(int[] nums,int key){
+		int low=0;
+		int high=nums.length-1;
+
+		while(low<=high){
+			int middle=low+((high-low)>>1);
+			if(nums[middle]>key){
+				high=middle-1;
+			}else if(nums[middle]<key){
+				low=middle+1;
+			}else 
+			return middle;
+		}
+		return -1;
+	}
 
 ##Implement Queue using stacks
 
 ##Implement Stack using Queues
 
 ##invert binary tree
+
+class TreeNode{
+	int val;
+	TreeNode left;
+	TreeNode right;
+	TreeNode(int x){val=x;}
+}
+
+public void invertTree(TreeNode root){
+	
+	if(root!=null){
+			if(root.left!=null||root.right!=null){
+				TreeNode tmp=root.left;
+				root.left=root.right;
+				root.right=tmp;
+			}
+
+			if(root.left!=null){
+				invertTree(root.left);
+			}
+
+			if(root.right!=null){
+				invertTree(root.right);
+			}
+	}
+}
+
+
 
 ##Swap two variables without using extra space
 
