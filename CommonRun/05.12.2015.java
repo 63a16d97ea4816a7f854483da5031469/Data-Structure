efdebfe50762e204afd90d1baf44cad6ea1653a5
Tony <ntutonycao@gmail.com>
2015-12-15 21:18:00 +0800

    Modified the content.

--- CommonRun/05.12.2015.java
+++ CommonRun/05.12.2015.java
@@ -618,7 +618,7 @@ public int findMaxSubArray(int[] nums){
 	int max_so_far=nums[0];
 
 	for(int i=1;i<nums.length;i++){
-		max_ending_here=Math.max(max_ending_here,max_ending_here+nums[i]);
+		max_ending_here=Math.max(nums[i],max_ending_here+nums[i]);
 		max_so_far=Math.max(max_so_far,max_ending_here);
 	}
 	return max_so_far;
--- CommonRun/13.12.2015-2.java
+++ CommonRun/13.12.2015-2.java
@@ -100,7 +100,7 @@ public int findMax(int[] nums){
 	int max_so_far=nums[0];
 
 	for(int i=1;i<nums.length;i++){
-		max_ending_here=Math.max(max_ending_here,max_ending_here+nums[i]);
+		max_ending_here=Math.max(nums[i],max_ending_here+nums[i]);
 		max_so_far=Math.max(max_ending_here,max_so_far);
 	}
 	return max_so_far;
