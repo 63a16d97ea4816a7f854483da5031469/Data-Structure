6a45d086add365329e3a1c06b84f8f86850196c3
63a16d97ea4816a7f854483da5031469 <ntutonycao@gmail.com>
2020-03-30 23:52:38 +0800

    Updated content.

--- LeetOJ/37. Sudoku Solver.java
+++ LeetOJ/37. Sudoku Solver.java
@@ -35,13 +35,73 @@ You may assume that the given Sudoku puzzle will have a single unique solution.
 The given board size is always 9x9.
 
 
-20 March 2020 at 6.49 pm
+30 March 2020 at //11.36pm-11.51pm （看题解写的）
  * 
  */
 
 
 
+class Solution {
+	//11.36pm-11.51pm （看题解写的）
+	public void solveSudoku(char[][] board) {
+		if (board == null || board.length == 0) return;
 
+		dfs(board);
+	}
+
+	boolean dfs(char[][] board) {
+
+		int n = board.length;
+
+		for (int i = 0; i<n; i++) {
+			for (int j = 0; j<n; j++) {
+				if (board[i][j] == '.') {
+					for (char num = '1'; num<= '9'; num++) {
+						if (isValid(board, i, j, num)) {
+							board[i][j] = num;
+							if (dfs(board)) {
+								return true;
+							} else {
+								board[i][j] = '.';
+							}
+						}
+					}
+					//如果9个数都试完了都不行，说明真不行
+					return false;
+				}
+
+			}
+		}
+		return true;
+	}
+
+	boolean isValid(char[][] board, int x, int y, char num) {
+		int n = board.length;
+
+		for (int j = 0; j<n; j++) {
+			if (board[x][j] == num) return false;
+
+		}
+
+		for (int i = 0; i<n; i++) {
+			if (board[i][y] == num) return false;
+		}
+
+		// check cube(小九宫格)
+		int rdx = x / 3 * 3;
+		int cdx = y / 3 * 3;
+
+		for (int i = rdx; i<rdx + 3; i++) {
+			for (int j = cdx; j<cdx + 3; j++) {
+				if (board[i][j] == num) {
+					return false;
+				}
+			}
+		}
+
+		return true;
+	}
+}
 
 
 
