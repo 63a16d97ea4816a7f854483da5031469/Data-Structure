5921a1ffcac689c95c1f906f35716a1645de4fe8
63a16d97ea4816a7f854483da5031469 <ntutonycao@gmail.com>
2020-04-04 23:41:59 +0800

    Updated content.

--- LeetOJ/62. Unique Paths.java
+++ LeetOJ/62. Unique Paths.java
@@ -49,8 +49,40 @@ It's guaranteed that the answer will be less than or equal to 2 * 10 ^ 9.
  */
 
 
+动态规划走法：
 
-
+class Solution {
+    //11.07pm-11.20pm 搜索耗时太长，超时。
+    //DFS 搜索
+    int count=0;
+    public int uniquePaths(int m, int n) {
+        if(m==0 || n==0) return 0;
+        if(m==1 || n==1) return 1;
+        
+        int[][] dp=new int[m][n];
+        
+        //只有一行
+        for(int i=1;i<n;i++){
+            dp[0][i]=1;
+        }
+        
+        //只有一列
+        for(int i=1;i<m;i++){
+            dp[i][0]=1;
+        }
+        
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i][j]=dp[i-1][j]+dp[i][j-1];
+            }
+        }
+  
+        return dp[m-1][n-1];
+    }
+    
+     
+    
+}
 
 
 
