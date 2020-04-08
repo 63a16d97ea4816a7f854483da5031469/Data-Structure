d9bfc9887baca4b9b08d33ea252747b0c7c56c71
63a16d97ea4816a7f854483da5031469 <ntutonycao@gmail.com>
2020-04-08 20:28:04 +0800

    Updated content.

--- LeetOJ/33. Search in Rotated Sorted Array.java
+++ LeetOJ/33. Search in Rotated Sorted Array.java
@@ -38,6 +38,15 @@ Output: -1
  */
 
 
+// 分析：如果直接遍历数组进行查找，那么时间复杂度O(n)。显然面试官是不会满意这样的答案的。其实这道题是二分查找的变体，因为数组进行了旋转，所以当我们切取一半的时候可能会出现误区，所以我们要做进一步的判断。具体来说，假设数组是nums，每次左边缘为low，右边缘为high，还有中间位置是mid。在每次迭代中，分三种情况：
+// （1）如果target==nums[mid]，那么mid就是我们要的结果，直接返回；
+// （2）如果nums[low]<=nums[mid]，那么说明从low到mid一定是有序的，那么我们只需要判断target是不是在low到mid之间，如果是则把右边缘移到mid-1，否则target就在另一半，即把左边缘移到mid+1。
+// （3）如果nums[low]>nums[mid]，那么说明从mid到high一定是有序的，那么我们只需要判断target是不是在mid到high之间，如果是则把左边缘移到mid+1，否则就target在另一半，即把右边缘移到mid-1。
+// ————————————————
+// 版权声明：本文为CSDN博主「北邮张博」的原创文章，遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接及本声明。
+// 原文链接：https://blog.csdn.net/Irving_zhang/article/details/78192831
+
+
 class Solution {
     // 7.55pm-8.24pm
 	public int search(int[] nums, int target) {
