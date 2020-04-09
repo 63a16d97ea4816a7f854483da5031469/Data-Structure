becb120eeafb4396bbfb167b6c544ee4e0ea84b2
63a16d97ea4816a7f854483da5031469 <ntutonycao@gmail.com>
2020-04-09 18:16:26 +0800

    Updated content.

--- LeetOJ/  Single Number.java
+++ LeetOJ/  Single Number.java
@@ -349,3 +349,53 @@ public class Solution {
     }
 
 }
+
+
+
+
+Single Number
+Solution
+Given a non-empty array of integers, every element appears twice except for one. Find that single one.
+
+Note:
+
+Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+
+Example 1:
+
+Input: [2,2,1]
+Output: 1
+Example 2:
+
+Input: [4,1,2,1,2]
+Output: 4
+
+
+
+class Solution {
+    //5.09pm - 5.15pm
+    public int singleNumber(int[] nums) {
+  
+        for(int i=1;i<nums.length;i++){
+           nums[0]= nums[0]^nums[i];
+        }
+        
+        return nums[0];
+    }
+}
+
+
+
+
+class Solution {
+    //5.09pm - 5.15pm
+    public int singleNumber(int[] nums) {
+ 
+        int count=nums[0];
+        for(int i=1;i<nums.length;i++){
+           count= count^nums[i];
+        }
+        
+        return count;
+    }
+}
