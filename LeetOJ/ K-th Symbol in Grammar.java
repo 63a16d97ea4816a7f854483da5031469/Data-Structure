42b97e201c5887cb5af9ea43844c9c20a2a85836
63a16d97ea4816a7f854483da5031469 <ntutonycao@gmail.com>
2020-04-11 19:27:40 +0800

    Updated content.

--- LeetOJ/ K-th Symbol in Grammar.java
+++ LeetOJ/ K-th Symbol in Grammar.java
@@ -44,10 +44,184 @@ K will be an integer in the range [1, 2^(N-1)].
 
 
 
+package leetcode;
+ 
+public class K_th_Symbol_in_Grammar_779 {
+ 
+	public int kthGrammar(int N, int K) {
+		if(K==1){
+			return 0;
+		}
+		if(K==2){
+			return 1;
+		}
+		if(K%2==1){
+			return kthGrammar(-1, (K+1)/2);
+		}
+		else{
+			return 1-kthGrammar(-1, K/2);
+		}
+	}
+ 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		K_th_Symbol_in_Grammar_779 k=new K_th_Symbol_in_Grammar_779();
+		System.out.println(k.kthGrammar(30, 434991989));	
+	}
+ 
+
+————————————————
+版权声明：本文为CSDN博主「huanghanqian」的原创文章，遵循 CC 4.0 BY-SA 版权协议，转载请附上原文出处链接及本声明。
+原文链接：https://blog.csdn.net/huanghanqian/article/details/79254001
 
 
 
 
+
+采用hashmap来加速，仍然不行：
+
+class Solution {
+    //5.51pm-6.06pm
+    //6.09pm-6.20pm
+    
+        String s="0";
+    HashMap<String, String> map=new HashMap<String,String>();
+//     public int kthGrammar(int N, int K) {
+     
+//         if(N==1 && K==1)  return 0;
+        
+//         for(int i=1;i<=N;i++){
+//             StringBuilder sb=new StringBuilder();
+//             for(char c:s.toCharArray()){
+//                 if(c=='0'){
+//                     sb.append("01");
+//                 }else if(c=='1'){
+//                     sb.append("10");
+//                 }else{
+//                     sb.append(c+"");
+//                 }
+//             }
+//             s=sb.toString();
+//         }
+        
+//        return  Integer.valueOf(s.charAt(K-1)+"");
+       
+//     }
+    
+       public int kthGrammar(int N, int K) {
+           
+           for(int i=1;i<=N;i++){
+              s= getNextStr(s);
+           }
+           
+          return  Integer.valueOf(s.charAt(K-1)+"");
+
+       }
+    
+    String getNextStr(String s){
+        
+        char c=s.charAt(0);
+        
+        if(s.length()==1){
+            if(c=='0') return "01";
+            else if(c=='1') return "10";
+            else return c+"";
+        }
+    
+        if(c=='0'){
+            
+            if(map.get(s.substring(1))==null){
+                return "01"+getNextStr(s.substring(1)); 
+            }else{
+                return "01"+map.get(s.substring(1));
+            }
+            
+            
+        }else if(c=='1'){
+            if(map.get(s.substring(1))==null){
+                return "10"+getNextStr(s.substring(1)); 
+            }else{
+                return "10"+map.get(s.substring(1));
+            }
+        }else{
+            if(map.get(s.substring(1))==null){
+                return ""+getNextStr(s.substring(1)); 
+            }else{
+                return ""+map.get(s.substring(1));
+            }
+        }
+    }
+ 
+    
+}
+
+
+
+超时：
+
+Submission Result: Time Limit Exceeded More Details 
+Last executed input:
+30
+434991989
+
+class Solution {
+    //5.51pm-6.06pm
+    //6.09pm-6.20pm
+    
+        String s="0";
+//     public int kthGrammar(int N, int K) {
+     
+//         if(N==1 && K==1)  return 0;
+        
+//         for(int i=1;i<=N;i++){
+//             StringBuilder sb=new StringBuilder();
+//             for(char c:s.toCharArray()){
+//                 if(c=='0'){
+//                     sb.append("01");
+//                 }else if(c=='1'){
+//                     sb.append("10");
+//                 }else{
+//                     sb.append(c+"");
+//                 }
+//             }
+//             s=sb.toString();
+//         }
+        
+//        return  Integer.valueOf(s.charAt(K-1)+"");
+       
+//     }
+    
+       public int kthGrammar(int N, int K) {
+           
+           for(int i=1;i<=N;i++){
+              s= getNextStr(s);
+           }
+           
+          return  Integer.valueOf(s.charAt(K-1)+"");
+
+       }
+    
+    String getNextStr(String s){
+        
+        char c=s.charAt(0);
+        
+        if(s.length()==1){
+            if(c=='0') return "01";
+            else if(c=='1') return "10";
+            else return c+"";
+        }
+    
+        if(c=='0'){
+            return "01"+getNextStr(s.substring(1));
+        }else if(c=='1'){
+            return "10"+getNextStr(s.substring(1));
+        }else{
+             return c+getNextStr(s.substring(1));
+        }
+    }
+ 
+    
+}
 
 
 
@@ -84,8 +258,6 @@ class Solution {
        return  Integer.valueOf(s.charAt(K-1)+"");
        
     }
- 
-    
 }
 
 
