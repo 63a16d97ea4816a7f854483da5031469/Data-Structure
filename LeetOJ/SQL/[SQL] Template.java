b87bc8e0df17ff1995d6480646dd0e32f469f94d
63a16d97ea4816a7f854483da5031469 <ntutonycao@gmail.com>
2020-05-02 17:40:48 +0800

    Updated content.

--- LeetOJ/SQL/[SQL] Template.java
+++ LeetOJ/SQL/[SQL] Template.java
@@ -1,25 +1,54 @@
 
 /*
  * 
-https://leetcode.com/problems/reverse-linked-list-ii/
+https://leetcode.com/problems/exchange-seats/
 
 
-Reverse a linked list from position m to n. Do it in-place and in one-pass.
+626. Exchange Seats
+Medium
 
-For example:
-Given 1->2->3->4->5->NULL, m = 2 and n = 4,
+282
 
-return 1->4->3->2->5->NULL.
+226
 
+Add to List
+
+Share
+SQL Schema
+Mary is a teacher in a middle school and she has a table seat storing students' names and their corresponding seat ids.
+
+The column id is continuous increment.
+ 
+
+Mary wants to change seats for the adjacent students.
+ 
+
+Can you write a SQL query to output the result for Mary?
+ 
+
++---------+---------+
+|    id   | student |
++---------+---------+
+|    1    | Abbot   |
+|    2    | Doris   |
+|    3    | Emerson |
+|    4    | Green   |
+|    5    | Jeames  |
++---------+---------+
+For the sample input, the output is:
+ 
+
++---------+---------+
+|    id   | student |
++---------+---------+
+|    1    | Doris   |
+|    2    | Abbot   |
+|    3    | Green   |
+|    4    | Emerson |
+|    5    | Jeames  |
++---------+---------+
 Note:
-Given m, n satisfy the following condition:
-1 ≤ m ≤ n ≤ length of list.
-
-public class Solution {
-    public ListNode reverseBetween(ListNode head, int m, int n) {
-        
-    }
-}
+If the number of students is odd, there is no need to change the last one's seat.
 
 12 April 2020 at 8:33: pm
 
@@ -40,8 +69,55 @@ public class Solution {
 
 
 
+Analysis
+For a score, its rank is the number of scores in the Scores table without duplicates that is larger than or equal to the score. Take above table as example, there are six scores, 3.50, 3.65, 4.00, 3.85, 4.00 and 3.65. The distinct scores are 3.50, 3.65, 3.85 and 4.00. For a score of 4.00, only 4.00 ≥ 4.00, so the rank is 1. Similarly, for a score of 3.65, 4.00 ≥ 3.65, 3.85 ≥ 3.65, 3.65 ≥ 3.65, therefore, the rank is 3.
 
+First, we need to get all distinct scores:
 
+1
+SELECT DISTINCT Score FROM Scores;
+Assuming that we call the table of distinct scores t, next we can fully join t and Scores and filter scores in t that is larger than or equal to each score in Scores:
+
+1
+2
+3
+SELECT s.Score, t.Score FROM
+(SELECT DISTINCT Score FROM Scores) AS t, Scores AS s
+WHERE s.Score <= t.Score;
+For each score in Scores, count how many scores in t are larger than or equal to it:
+
+1
+2
+3
+4
+SELECT s.Score, COUNT(t.Score) AS Rank FROM
+(SELECT DISTINCT Score FROM Scores) AS t, Scores AS s
+WHERE s.Score <= t.Score
+GROUP BY s.Id, s.Score;
+Finally, sort by score in descending order:
+
+1
+2
+3
+4
+5
+SELECT s.Score, COUNT(t.Score) AS Rank FROM
+(SELECT DISTINCT Score FROM Scores) AS t, Scores AS s
+WHERE s.Score <= t.Score
+GROUP BY s.Id, s.Score
+ORDER BY s.Score DESC;
+Solution
+1
+2
+3
+4
+5
+SELECT s.Score, COUNT(t.Score) AS Rank FROM
+(SELECT DISTINCT Score FROM Scores) AS t, Scores AS s
+WHERE s.Score <= t.Score
+GROUP BY s.Id, s.Score
+ORDER BY s.Score DESC;
+Link
 
 
 
