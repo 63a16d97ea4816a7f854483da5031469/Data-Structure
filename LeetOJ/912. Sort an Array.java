f4128c693f500ccabe9ea725c324f38d8a30ccfe
63a16d97ea4816a7f854483da5031469 <ntutonycao@gmail.com>
2020-05-04 20:39:21 +0800

    Updated content.

---
+++ LeetOJ/912. Sort an Array.java
@@ -0,0 +1,80 @@
+
+/*
+ * 
+https://leetcode.com/problems/sort-an-array/
+
+
+912. Sort an Array
+Medium
+
+349
+
+228
+
+Add to List
+
+Share
+Given an array of integers nums, sort the array in ascending order.
+
+ 
+
+Example 1:
+
+Input: nums = [5,2,3,1]
+Output: [1,2,3,5]
+Example 2:
+
+Input: nums = [5,1,1,2,0,0]
+Output: [0,0,1,1,2,5]
+ 
+
+Constraints:
+
+1 <= nums.length <= 50000
+-50000 <= nums[i] <= 50000
+
+12 April 2020 at 8:33: pm
+
+
+对题目易错地方进行总结:
+
+
+对题目的实现思路进行几句话总结:
+
+
+从这道题目学到了什么，哪些地方需要提升? :
+
+
+
+
+ * 
+ */
+
+
+
+
+class Solution {
+    public int[] sortArray(int[] nums) {
+        Arrays.sort(nums);
+        return nums;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
