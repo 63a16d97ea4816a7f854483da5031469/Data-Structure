37c8adbe3419e913c71ab0027d3403a565ee74b2
63a16d97ea4816a7f854483da5031469 <ntutonycao@gmail.com>
2020-05-06 13:55:31 +0800

    Updated content.

---
+++ LeetOJ/Linked List Cycle.java
@@ -0,0 +1,106 @@
+
+/*
+ * 
+https://leetcode.com/explore/learn/card/linked-list/214/two-pointer-technique/1212/
+
+
+  Linked List Cycle
+Solution
+Given a linked list, determine if it has a cycle in it.
+
+To represent a cycle in the given linked list, we use an integer pos which represents the position (0-indexed) in the linked list where tail connects to. If pos is -1, then there is no cycle in the linked list.
+
+ 
+
+Example 1:
+
+Input: head = [3,2,0,-4], pos = 1
+Output: true
+Explanation: There is a cycle in the linked list, where tail connects to the second node.
+
+
+Example 2:
+
+Input: head = [1,2], pos = 0
+Output: true
+Explanation: There is a cycle in the linked list, where tail connects to the first node.
+
+
+Example 3:
+
+Input: head = [1], pos = -1
+Output: false
+Explanation: There is no cycle in the linked list.
+
+
+ 
+
+Follow up:
+
+Can you solve it using O(1) (i.e. constant) memory?
+
+12 April 2020 at 8:33: pm
+
+
+对题目易错地方进行总结:
+
+
+对题目的实现思路进行几句话总结:
+
+
+从这道题目学到了什么，哪些地方需要提升? :
+
+
+
+
+ * 
+ */
+
+
+
+
+
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        
+        while(fast!=null&&fast.next!=null){ // this one is important.
+            slow=slow.next;
+            fast=fast.next.next;
+            
+            if(slow==fast) return true;
+        }
+        
+        return false;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
