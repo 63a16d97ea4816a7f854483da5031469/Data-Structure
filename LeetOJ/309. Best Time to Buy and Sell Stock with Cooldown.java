e86279139f4d06e3485c0e60c0fdab9f31e53f7a
63a16d97ea4816a7f854483da5031469 <ntutonycao@gmail.com>
2020-05-09 23:59:35 +0800

    Updated content.

--- LeetOJ/309. Best Time to Buy and Sell Stock with Cooldown.java
+++ LeetOJ/309. Best Time to Buy and Sell Stock with Cooldown.java
@@ -43,11 +43,30 @@ https://soulmachine.gitbooks.io/algorithm-essentials/java/dp/best-time-to-buy-an
  * 
  */
 
+// Space: O(1)
+ // Best Time to Buy and Sell Stock with Cooldown
+// Time Complexity: O(n), Space Complexity: O(1)
+public class Solution {
+    public int maxProfit(int[] prices) {
+        if (prices == null || prices.length == 0) return 0;
+
+        int curSell = 0;   // sell[i]
+        int prevSell = 0;  // sell[i-2]
+        int buy = -prices[0]; // buy[i]
+
+        for (int i = 1; i < prices.length; ++i) {
+            final int tmp = curSell;
+            curSell = Math.max(curSell, buy + prices[i]);
+            buy = Math.max(buy, (i > 1 ? prevSell : 0) - prices[i]);
+            prevSell = tmp;
+        }
+        return curSell;
+    }
+}
 
 
 
-
-
+// Space: O(n)
 // https://soulmachine.gitbooks.io/algorithm-essentials/java/dp/best-time-to-buy-and-sell-stock-with-cooldown.html
 // Best Time to Buy and Sell Stock with Cooldown
 // Time Complexity: O(n), Space Complexity: O(n)
