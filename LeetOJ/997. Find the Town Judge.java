491643c7bf0bf49b7b883e2644ac2ad7e11fbfc3
63a16d97ea4816a7f854483da5031469 <ntutonycao@gmail.com>
2020-05-10 19:21:53 +0800

    Updated content.

--- LeetOJ/997. Find the Town Judge.java
+++ LeetOJ/997. Find the Town Judge.java
@@ -76,8 +76,55 @@ trust[i][0] != trust[i][1]
 
 
 
+// 速度最快解法:
+class Solution {
+    public int findJudge(int N, int[][] trust) {
+         if(trust.length<  N-1){
+            return -1;
+        }
+        
+        int [] inEdges = new int[N+1];
+        int [] outEdges =new int[N+1];
+        for(int[] relation :trust){
+            outEdges[relation[0]]++;
+            inEdges[relation[1]]++;
+        }
+        for (int i = 1; i <= N; i++) {
+        if (inEdges[i] == N - 1 && outEdges[i] == 0) {
+            return i;
+        }
+    }
+        return -1;
+       
+    }
+}
 
 
+
+class Solution {
+    public int findJudge(int N, int[][] trust) 
+	{
+    	int[] count = new int[N];
+        int[] trusted = new int[N]; 
+        
+        for(int i = 0; i < trust.length; i++)
+        {
+            int a = trust[i][0]; 
+            int b = trust[i][1];
+            count[a - 1]++;
+            trusted[b - 1]++;
+        }
+        
+        for(int i = 0; i < N; i++)
+        {
+            if(count[i] == 0 && trusted[i] == N - 1)
+            {
+                return i + 1;
+            }    
+        }
+        return -1;
+	}
+}
 
 
 class Solution {
