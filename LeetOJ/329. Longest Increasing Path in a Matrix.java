fc71d0e27732dba6784144aafca18c959757e4d7
63a16d97ea4816a7f854483da5031469 <ntutonycao@gmail.com>
2020-05-17 00:05:52 +0800

    Updated content.

--- LeetOJ/329. Longest Increasing Path in a Matrix.java
+++ LeetOJ/329. Longest Increasing Path in a Matrix.java
@@ -39,7 +39,9 @@ Input: nums =
 Output: 4 
 Explanation: The longest increasing path is [3, 4, 5, 6]. Moving diagonally is not allowed.
 
-12 April 2020 at 8:33: pm
+16 May 2020 at 11:16 pm
+
+
 
 
 对题目易错地方进行总结:
@@ -52,11 +54,11 @@ Explanation: The longest increasing path is [3, 4, 5, 6]. Moving diagonally is n
 
 
 
-
  * 
  */
 
 
+// 这道题给我们一个二维数组，让我们求矩阵中最长的递增路径，规定我们只能上下左右行走，不能走斜线或者是超过了边界。那么这道题的解法要用递归和DP来解，用DP的原因是为了提高效率，避免重复运算。我们需要维护一个二维动态数组dp，其中dp[i][j]表示数组中以(i,j)为起点的最长递增路径的长度，初始将dp数组都赋为0，当我们用递归调用时，遇到某个位置(x, y), 如果dp[x][y]不为0的话，我们直接返回dp[x][y]即可，不需要重复计算。我们需要以数组中每个位置都为起点调用递归来做，比较找出最大值。在以一个位置为起点用DFS搜索时，对其四个相邻位置进行判断，如果相邻位置的值大于上一个位置，则对相邻位置继续调用递归，并更新一个最大值，搜素完成后返回即可
 
 
 
