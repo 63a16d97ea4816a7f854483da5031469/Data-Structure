a3e3bb2bc012fac1ca22ce2642ea2bbd38e2174b
63a16d97ea4816a7f854483da5031469 <ntutonycao@gmail.com>
2020-05-17 14:07:40 +0800

    Updated content.

--- LeetOJ/670. Maximum Swap.java
+++ LeetOJ/670. Maximum Swap.java
@@ -1,32 +1,37 @@
 
 /*
  * 
-https://leetcode.com/problems/map-sum-pairs/
+https://leetcode.com/problems/maximum-swap/
 
 
-677. Map Sum Pairs
+670. Maximum Swap
 Medium
 
-440
+855
 
-75
+62
 
 Add to List
 
 Share
-Implement a MapSum class with insert, and sum methods.
-
-For the method insert, you'll be given a pair of (string, integer). The string represents the key and the integer represents the value. If the key already existed, then the original key-value pair will be overridden to the new one.
-
-For the method sum, you'll be given a string representing the prefix, and you need to return the sum of all the pairs' value whose key starts with the prefix.
+Given a non-negative integer, you could swap two digits at most once to get the maximum valued number. Return the maximum valued number you could get.
 
 Example 1:
-Input: insert("apple", 3), Output: Null
-Input: sum("ap"), Output: 3
-Input: insert("app", 2), Output: Null
-Input: sum("ap"), Output: 5
 
-17 May 2020 at 12:01 pm
+Input: 2736
+Output: 7236
+Explanation: Swap the number 2 and the number 7.
+Example 2:
+
+Input: 9973
+Output: 9973
+Explanation: No swap.
+Note:
+
+The given number is in the range [0, 108]
+
+17 May 2020 at 1:53 pm
+
 
 对题目易错地方进行总结:
 
@@ -36,27 +41,130 @@ Input: sum("ap"), Output: 5
 
 从这道题目学到了什么，哪些地方需要提升? :
 
+
+
+
  * 
  */
 
 
+// 需要找最大的整数， 那么被替换的数字需要是最左边的
+// 对于某一个被替换的数字， 需要找右边最大的数字
+
+
+// 这个思路有想到，但是想不到如何去实施，写出code，所以看了题解
+
+
+public int maximumSwap(int num) {
+    char[] arr = String.valueOf(num).toCharArray();
+    for(int i = 0; i < arr.length; ++i) {
+        int maxIndex = i;
+        for(int j = arr.length-1; j > i; --j) {
+            if(arr[j] > arr[maxIndex]) {
+                maxIndex = j;
+            }
+        }
+        if(maxIndex > i) {
+            char t = arr[i];
+            arr[i] = arr[maxIndex];
+            arr[maxIndex] = t;
+            return Integer.valueOf(new String(arr));
+        }
+    }
+    return num;
+}
+
+
+class Solution {
+    public int maximumSwap(int num) {
+        char[] A = Integer.toString(num).toCharArray();
+        int[] last = new int[10];
+        //建立数字与坐标映射
+        for (int i = 0; i < A.length; i++) {
+            last[A[i] - '0'] = i;
+        }
+
+        for (int i = 0; i < A.length; i++) {
+            for (int d = 9; d > A[i] - '0'; d--) {
+                if (last[d] > i) {
+                    char tmp = A[i];
+                    A[i] = A[last[d]];
+                    A[last[d]] = tmp;
+                    return Integer.valueOf(new String(A));
+                }
+            }
+        }
+        return num;
+    }
+}
+
+
+
+class Solution {
+    public int maximumSwap(int num) {
+        String numStr = num + "";
+        char[] nums = numStr.toCharArray();
+        int left = -1, right = -1;
+        
+        int max = nums[nums.length -1];
+        int max_id = nums.length - 1;
+        
+        for(int i = nums.length - 2; i >= 0; i--) {
+            if(nums[i] > max) {
+                max = nums[i];
+                max_id = i;
+            }
+            
+            if(nums[i] < max) {
+                left = i;
+                right = max_id;
+            }
+        }
+        
+        if(left != -1) {
+            char temp = nums[left];
+            nums[left] = nums[right];
+            nums[right] = temp;
+        }
+        
+        String newNumStr = new String(nums);
+        return Integer.valueOf(newNumStr);
+    }
+}
 
 
 
 
+// WA:
+// Input
+// 98368
+// Output
+// 98638
+// Expected
+// 98863
 
-
-
-
-
-
-
-
-
-
-
-
-
+class Solution {
+    //1.45pm-1.53pm
+    //使用单调栈思想
+    public int maximumSwap(int num) {
+        String str=num+"";
+        if(str.length()<=1) return num;
+        char[] c=str.toCharArray();
+        for(int i=0;i<str.length()-1;i++){
+            if(Integer.parseInt(c[i]+"")<Integer.parseInt(c[i+1]+"")){
+                char tmp=c[i];
+                c[i]=c[i+1];
+                c[i+1]=tmp;
+                break;
+            }
+        }
+        String s="";
+        for(int i=0;i<c.length;i++){
+           s+=c[i]; 
+        }
+        return Integer.parseInt(s);
+    }
+}
 
 
 
