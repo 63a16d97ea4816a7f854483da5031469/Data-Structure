be96a9ef025f36425a5c2b9277611f8a212cee4b
63a16d97ea4816a7f854483da5031469 <ntutonycao@gmail.com>
2020-05-23 22:55:36 +0800

    Updated content.

--- LeetOJ/215. Kth Largest Element in an Array.java
+++ LeetOJ/215. Kth Largest Element in an Array.java
@@ -28,6 +28,21 @@ You may assume k is always valid, 1 ≤ k ≤ array's length.
 
 
 5 April 2020 at 5.54pm-6.02pm 尝试使用堆来解决
+
+
+
+https://aaronice.gitbook.io/system-design/
+
+https://aaronice.gitbook.io/lintcode/
+
+https://aaronice.gitbook.io/lintcode/problem-solving-summary/java-interview-tips/data-structures-in-java
+
+https://github.com/aaronice/CS-Notes
+
+
+学习英语的工具：
+https://elsaspeak.com/en/
+
  * 
  */
 
@@ -82,6 +97,48 @@ class Solution {
 
 
 
+class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        int left = 0;
+        int right = nums.length - 1;
+        int indexOfKthLargest = nums.length - k;
+        while (left <= right) {
+            int pivot = partition(nums, left, right);
+            if (pivot == indexOfKthLargest) {
+                return nums[pivot];
+            } else if (pivot > indexOfKthLargest) {
+                right = pivot - 1;
+            } else {
+                left = pivot + 1;
+            }
+        }
+        return 0;
+    }
+    
+    private int partition(int[] nums, int start, int end) {
+        int pivot = nums[end];
+        int i = start;
+        int j = start;
+        while (j < end) {
+            if (nums[j] < pivot) {
+                swap(nums, i, j);
+                i++;
+                j++;
+            } else {
+                j++;
+            }
+        }
+        swap(nums, i, end);
+        
+        return i;
+    }
+    
+    private void swap(int[] a, int i, int j) {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+    }
+}
 
 
 
@@ -89,8 +146,30 @@ class Solution {
 
 
 
-
-
+class Solution {
+    public:
+        int findKthLargest(vector<int>& nums, int k) {
+            int left = 0, right = nums.size() - 1;
+            while (true) {
+                int pos = partition(nums, left, right);
+                if (pos == k - 1) return nums[pos];
+                if (pos > k - 1) right = pos - 1;
+                else left = pos + 1;
+            }
+        }
+        int partition(vector<int>& nums, int left, int right) {
+            int pivot = nums[left], l = left + 1, r = right;
+            while (l <= r) {
+                if (nums[l] < pivot && nums[r] > pivot) {
+                    swap(nums[l++], nums[r--]);
+                }
+                if (nums[l] >= pivot) ++l;
+                if (nums[r] <= pivot) --r;
+            }
+            swap(nums[left], nums[r]);
+            return r;
+        }
+    };
 
 
 
