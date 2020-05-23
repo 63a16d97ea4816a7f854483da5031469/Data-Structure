c261c2682792e4795de1148e4bd43700e003300a
63a16d97ea4816a7f854483da5031469 <ntutonycao@gmail.com>
2020-05-23 19:08:13 +0800

    Updated content.

---
+++ LeetOJ/Design HashSet.java
@@ -0,0 +1,154 @@
+
+/*
+ * 
+https://leetcode.com/problems/reverse-linked-list-ii/
+
+
+705. Design HashSet
+Easy
+
+290
+
+68
+
+Add to List
+
+Share
+Design a HashSet without using any built-in hash table libraries.
+
+To be specific, your design should include these functions:
+
+add(value): Insert a value into the HashSet. 
+contains(value) : Return whether the value exists in the HashSet or not.
+remove(value): Remove a value in the HashSet. If the value does not exist in the HashSet, do nothing.
+
+Example:
+
+MyHashSet hashSet = new MyHashSet();
+hashSet.add(1);         
+hashSet.add(2);         
+hashSet.contains(1);    // returns true
+hashSet.contains(3);    // returns false (not found)
+hashSet.add(2);          
+hashSet.contains(2);    // returns true
+hashSet.remove(2);          
+hashSet.contains(2);    // returns false (already removed)
+
+Note:
+
+All values will be in the range of [0, 1000000].
+The number of operations will be in the range of [1, 10000].
+Please do not use the built-in HashSet library.
+
+
+
+23 May 2020 at 8:33: pm
+
+
+对题目易错地方进行总结:
+
+
+对题目的实现思路进行几句话总结:
+
+
+从这道题目学到了什么，哪些地方需要提升? :
+
+
+
+
+ * 
+ */
+
+
+
+
+
+
+
+//7.03pm-7.05pm 使用了其他内建的lib:
+class MyHashSet {
+    LinkedList<Integer> list=null;
+    /** Initialize your data structure here. */
+    public MyHashSet() {
+        list=new LinkedList<Integer>();
+    }
+    
+    public void add(int key) {
+        if(!list.contains(key)){
+            list.add(key);
+        }
+    }
+    
+    public void remove(int key) {
+        if(list.contains(key)){
+            list.remove(new Integer(key));
+        }
+    }
+    
+    /** Returns true if this set contains the specified element */
+    public boolean contains(int key) {
+        return list.contains(key);
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
+
+
+
+
+
+
+class MyHashSet {
+    
+    boolean[] arr = new boolean[100];// start with 100 elements for fast initialization
+    /** Initialize your data structure here. */
+    public MyHashSet() {
+        
+    }
+    
+    public void add(int key) {
+        if(key>=arr.length) // if array is too small to accomodate key, extend it.
+            extend(key);
+        arr[key]=true;
+    }
+    
+    public void remove(int key) {
+        if(key>=arr.length) // if array is too small to accomodate key, extend it.
+            extend(key);
+        arr[key]=false;
+    }
+    
+    /** Returns true if this set contains the specified element */
+    public boolean contains(int key) {
+        if(key>=arr.length) // key cannot be in array if array's length < key
+            return false;
+        return arr[key]==true;
+    }
+    
+    public void extend(int key){
+        arr= Arrays.copyOf(arr, key+2);   
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
+
+
+
+
+
+
+
+
+
