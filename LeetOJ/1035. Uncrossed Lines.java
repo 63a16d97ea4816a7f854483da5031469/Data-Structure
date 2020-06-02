0df5b81a27bf4316effd752ae6e8341bda2ee143
63a16d97ea4816a7f854483da5031469 <ntutonycao@gmail.com>
2020-06-03 00:22:57 +0800

    Updated content.

--- LeetOJ/1035. Uncrossed Lines.java
+++ LeetOJ/1035. Uncrossed Lines.java
@@ -97,7 +97,28 @@ class Solution {
 
 
 
+class Solution {
+    public int maxUncrossedLines(int[] A, int[] B) {
+        int n1 = A.length;
+        int n2 = B.length;
+        int[][] dp = new int[n1+1][n2+1];
+        for(int i=0;i<n1;i++){
+            for(int j=0;j<n2;j++){
+                if(A[i]==B[j]){
+                    dp[i+1][j+1] = dp[i][j]+1;
+                }else{
+                    dp[i+1][j+1] = Math.max(dp[i+1][j],dp[i][j+1]);
+                }
+            }
+        }
+        return dp[n1][n2];
+    }
+}
 
+// 作者：lazyant
+// 链接：https://leetcode-cn.com/problems/uncrossed-lines/solution/dong-tai-gui-hua-by-lazyant/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 
 
 
