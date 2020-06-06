1ec5e52ed10fe286f9ad97361d11d4815b32b76c
63a16d97ea4816a7f854483da5031469 <ntutonycao@gmail.com>
2020-06-06 23:56:12 +0800

    Updated content.

--- LeetOJ/[Shell]Word Frequency.java
+++ LeetOJ/[Shell]Word Frequency.java
@@ -77,6 +77,36 @@ cat words.txt | tr -s ' ' '\n'| sort | uniq -c | sort -r | awk '{print $2, $1}'
 
 
 
+解法一：
+
+grep -oE '[a-z]+' words.txt | sort | uniq -c | sort -nr | awk '{print $2" "$1}' 
+ 
+
+ 
+
+下面这种方法用的关键字是tr命令，该命令主要用来进行字符替换或者大小写替换，详解请参见这个帖子。后面紧跟的-s参数表示如果发现连续的字符，就把它们缩减为1个，而后面的‘ ’和‘\n'就是空格和回车，意思是把所有的空格都换成回车，那么第一段命令tr -s ' ' '\n' < words.txt 就好理解了，将单词之间的空格都换成回车，跟上面的第一段实现的作用相同，后面就完全一样了，参见上面的讲解：
+
+ 
+
+解法二：
+
+tr -s ' ' '\n' < words.txt | sort | uniq -c | sort -nr | awk '{print $2, $1}'
+ 
+
+ 
+
+下面这种方法就没有用管道命令进行一行写法了，而是使用了强大的文本分析工具awk进行写类C的代码来实现，这种写法在之前的那道Transpose File已经讲解过了，这里就不多说了，最后要注意的是sort命令的参数-nr -k 2表示按第二列的降序数值排列：
+
+ 
+
+解法三：
+
+awk '{
+    for (i = 1; i <= NF; ++i) ++s[$i];
+} END {
+    for (i in s) print i, s[i];
+}' words.txt | sort -nr -k 2
+ 
 
 
 
