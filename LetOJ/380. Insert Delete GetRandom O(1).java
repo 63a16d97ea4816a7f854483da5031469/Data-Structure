2c423c59ac26c31871f06b9d332edd0625ad3d0c
63a16d97ea4816a7f854483da5031469 <ntutonycao@gmail.com>
2020-06-13 00:11:19 +0800

    Updated content.

--- LetOJ/380. Insert Delete GetRandom O(1).java
+++ LetOJ/380. Insert Delete GetRandom O(1).java
@@ -169,6 +169,79 @@ class RandomizedSet {
 
 
 
+class RandomizedSet {
+    List<Integer> list = new ArrayList<>();
+    Map<Integer, Integer> idxMap = new HashMap<>();
+    Random random = new Random();
+    /** Initialize your data structure here. */
+    public RandomizedSet() {
+        
+    }
+    
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    public boolean insert(int val) {
+        if (idxMap.containsKey(val)) {
+            return false;
+        }
+        list.add(val);
+        idxMap.put(val, idxMap.size());
+        return true;
+    }
+    
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    public boolean remove(int val) {
+        if (!idxMap.containsKey(val)) {
+            return false;
+        }
+         
+        int indexRemove = idxMap.get(val);
+        int tail = list.get(list.size() - 1);
+         
+        swap(indexRemove, list.size() - 1);
+        idxMap.put(tail, indexRemove);
+        list.remove(list.size() - 1);
+        idxMap.remove(val);
+         
+        return true;
+    }
+    
+    /** Get a random element from the set. */
+    public int getRandom() {
+        if (idxMap.isEmpty()) {
+            return 0;
+        }
+  
+        int rid = random.nextInt(idxMap.size());
+        return list.get(rid);
+    }
+    private void swap(int i, int j) {
+        int temp = list.get(i);
+        list.set(i, list.get(j));
+        list.set(j, temp);
+    }
+}
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
+
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * boolean param_1 = obj.insert(val);
+ * boolean param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
+
+
+
+
+
+
 // import java.util.*;
 class RandomizedSet {
     //11.21pm-11.44pm
