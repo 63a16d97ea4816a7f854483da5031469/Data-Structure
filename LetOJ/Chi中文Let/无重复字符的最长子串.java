c48bcb3f69f9c0cf51839a4066189a166f656e17
63a16d97ea4816a7f854483da5031469 <ntutonycao@gmail.com>
2020-07-10 10:19:10 +0800

    Updated content.

--- LetOJ/Chi中文Let/无重复字符的最长子串.java
+++ LetOJ/Chi中文Let/无重复字符的最长子串.java
@@ -62,6 +62,22 @@ class Solution {
 
 
 
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        HashSet<Character> set=new HashSet<Character>();
+        int max=0;
+        for(int i=0, j=0;i<s.length();i++){
+            if(set.contains(s.charAt(i))){
+                while(set.contains(s.charAt(i))){
+                    set.remove(s.charAt(j++));
+                }
+            }
+            set.add(s.charAt(i));
+            max=Math.max(max,i-j+1);
+        }
+        return max;
+    }
+}
 
 
 
