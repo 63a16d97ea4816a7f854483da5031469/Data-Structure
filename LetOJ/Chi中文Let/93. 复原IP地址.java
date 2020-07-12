22f542116cf843373773ecfe2fab853479f25158
63a16d97ea4816a7f854483da5031469 <ntutonycao@gmail.com>
2020-07-12 20:04:03 +0800

    Updated content.

--- LetOJ/Chi中文Let/93. 复原IP地址.java
+++ LetOJ/Chi中文Let/93. 复原IP地址.java
@@ -92,7 +92,40 @@ class Solution {
 
 
 
+class Solution {
+  public List<String> restoreIpAddresses(String s) {
+  List<String> ret = new ArrayList<>();
 
+  StringBuilder ip = new StringBuilder();
+  
+  for(int a = 1 ; a < 4 ; ++ a)
+      for(int b = 1 ; b < 4 ; ++ b)
+          for(int c = 1 ; c < 4 ; ++ c)
+              for(int d = 1 ; d < 4 ; ++ d)
+              {
+                  if(a + b + c + d == s.length() )
+                  {
+                      int n1 = Integer.parseInt(s.substring(0, a));
+                      int n2 = Integer.parseInt(s.substring(a, a+b));
+                      int n3 = Integer.parseInt(s.substring(a+b, a+b+c));
+                      int n4 = Integer.parseInt(s.substring(a+b+c));
+                      if(n1 <= 255 && n2 <= 255 && n3 <= 255 && n4 <= 255)
+                      {
+                          ip.append(n1).append('.').append(n2)
+                                  .append('.').append(n3).append('.').append(n4);
+                          if(ip.length() == s.length() + 3) ret.add(ip.toString());
+                          ip.delete(0, ip.length());
+                      }
+                  }
+              }
+  return ret;
+}
+
+// 作者：reals
+// 链接：https://leetcode-cn.com/problems/restore-ip-addresses/solution/ke-neng-shi-zui-you-mei-de-bao-li-jie-fa-liao-by-u/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+}
 
 
 
