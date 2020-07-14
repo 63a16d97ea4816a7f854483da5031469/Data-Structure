3f2f87849a96a9f8c9bc2091c907faa221fe1beb
63a16d97ea4816a7f854483da5031469 <ntutonycao@gmail.com>
2020-07-14 23:34:24 +0800

    Updated content.

---
+++ LetOJ/Chi中文Let/21. 合并两个有序链表.java
@@ -0,0 +1,136 @@
+
+/*
+ * 
+link: 
+
+
+2020-7-1 at 8:33 pm
+
+
+对题目易错地方进行总结:
+
+
+对题目的实现思路进行几句话总结:
+
+
+从这道题目学到了什么，哪些地方需要提升? :
+
+
+
+ * 
+ */
+
+
+
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    //11.22pm-11.31pm
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode finalList=new ListNode(-1);
+        ListNode head=finalList;
+        while(l1!=null&&l2!=null){
+            if(l1.val>l2.val){
+                head.next=new ListNode(l2.val);
+                l2=l2.next;
+            }else{
+                head.next=new ListNode(l1.val);
+                l1=l1.next;
+            }
+            head=head.next;
+        }
+
+        while(l1!=null){
+            head.next=new ListNode(l1.val);
+            l1=l1.next;
+            head=head.next;
+        }
+        while(l2!=null){
+            head.next=new ListNode(l2.val);
+            l2=l2.next;
+            head=head.next;
+        }
+        return finalList.next;
+    }
+}
+
+
+
+
+
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        if (l1 == null) {
+            return l2;
+        }
+        else if (l2 == null) {
+            return l1;
+        }
+        else if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        }
+        else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
+        }
+
+    }
+}
+
+// 作者：LeetCode-Solution
+// 链接：https://leetcode-cn.com/problems/merge-two-sorted-lists/solution/he-bing-liang-ge-you-xu-lian-biao-by-leetcode-solu/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+
+
+
+
+//迭代:
+// 思路
+
+// 我们可以用迭代的方法来实现上述算法。当 l1 和 l2 都不是空链表时，判断 l1 和 l2 哪一个链表的头节点的值更小，将较小值的节点添加到结果里，当一个节点被添加到结果里之后，将对应链表中的节点向后移一位。
+
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        ListNode prehead = new ListNode(-1);
+
+        ListNode prev = prehead;
+        while (l1 != null && l2 != null) {
+            if (l1.val <= l2.val) {
+                prev.next = l1;
+                l1 = l1.next;
+            } else {
+                prev.next = l2;
+                l2 = l2.next;
+            }
+            prev = prev.next;
+        }
+
+        // 合并后 l1 和 l2 最多只有一个还未被合并完，我们直接将链表末尾指向未合并完的链表即可
+        prev.next = l1 == null ? l2 : l1;
+
+        return prehead.next;
+    }
+}
+
+// 作者：LeetCode-Solution
+// 链接：https://leetcode-cn.com/problems/merge-two-sorted-lists/solution/he-bing-liang-ge-you-xu-lian-biao-by-leetcode-solu/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+
+
+
+
