18809c09027ae0c86f0ce1863c55a8ef2981f2ce
63a16d97ea4816a7f854483da5031469 <ntutonycao@gmail.com>
2020-07-18 10:41:40 +0800

    Updated content.

--- LetOJ/Chi中文Let/142. 环形链表 II.java
+++ LetOJ/Chi中文Let/142. 环形链表 II.java
@@ -61,8 +61,50 @@ https://leetcode-cn.com/problems/linked-list-cycle-ii/
 
 
 
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    //10.31am-10.41am
+    public ListNode detectCycle(ListNode head) {
+        if(head==null||head.next==null) return null;
+        ListNode fast=head;
+        ListNode slow=head;
 
- 
+        while(fast!=null&&fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+               break;
+            }
+        }
+        if(head.next!=null){
+            slow=head;
+            while(slow!=null&&fast!=null){
+                if(slow==fast){
+                    return slow;
+                }
+                slow=slow.next;
+                fast=fast.next;
+            }
+        }
+        return null;
+    }
+}
+
+// 作者：LeetCode
+// 链接：https://leetcode-cn.com/problems/linked-list-cycle-ii/solution/huan-xing-lian-biao-ii-by-leetcode/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
 
 
 
