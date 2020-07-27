c7deb0723a604bb549449bf766857c91e01ab685
63a16d97ea4816a7f854483da5031469 <ntutonycao@gmail.com>
2020-07-27 18:00:32 +0800

    Updated content.

--- LetOJ/Chi中文Let/221. 最大正方形.java
+++ LetOJ/Chi中文Let/221. 最大正方形.java
@@ -119,19 +119,70 @@ class Solution {
 
 
 
+//改变了一下：
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        int maxSide = 0;
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return maxSide;
+        }
+        int rows = matrix.length, columns = matrix[0].length;
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                if (matrix[i][j] == '1') {
+                    // 遇到一个 1 作为正方形的左上角
+                    maxSide = Math.max(maxSide, 1);
+                    // 计算可能的最大正方形边长
+                    int currentMaxSide = Math.min(rows - i, columns - j);
+                    for (int k = 1; k < currentMaxSide; k++) {
+                        // 判断新增的一行一列是否均为 1
+                        boolean flag = true;
+                        //这个是为了加速的，就是先判断右下角，如果不行就不做后面的了
+                        // if (matrix[i + k][j + k] == '0') {
+                        //     break;
+                        // }
+                        for (int m = 0; m < k+1; m++) {
+                            if (matrix[i + k][j + m] == '0' || matrix[i + m][j + k] == '0') {
+                                flag = false;
+                                break;
+                            }
+                        }
+                        if (flag) {
+                            maxSide = Math.max(maxSide, k + 1);
+                        } else {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        int maxSquare = maxSide * maxSide;
+        return maxSquare;
+    }
+}
 
 
 
+ 
 
-
-
-
-
-
-
-
-
-
-
-
-
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        if(matrix==null||matrix.length==0||matrix[0].length==0) return 0;
+        int max=0;
+        int row=matrix.length, col=matrix[0].length;
+        int[][] dp=new int[row][col];
+        for(int i=0;i<row;i++){
+            for(int j=0;j<col;j++){
+                if(matrix[i][j]=='1'){
+                    if(i==0||j==0){
+                        dp[i][j]=1;
+                    }else{
+                        dp[i][j]=Math.min(Math.min(dp[i-1][j],dp[i][j-1]),dp[i-1][j-1])+1;
+                    }
+                    max=Math.max(max,dp[i][j]);
+                }
+            }
+        }
+        return max*max;
+    }
+}
