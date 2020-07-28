819ca55e0515bdc23d831ef05edcb0f106c775e7
63a16d97ea4816a7f854483da5031469 <ntutonycao@gmail.com>
2020-07-28 17:01:34 +0800

    Updated content.

---
+++ LetOJ/Chi中文Let/【SQL】第二高的薪水.java
@@ -0,0 +1,73 @@
+
+/*
+ * 
+link: 
+https://leetcode-cn.com/problems/second-highest-salary/
+
+2020-7-28 at 5:01 pm
+
+
+对题目易错地方进行总结:
+
+
+对题目的实现思路进行几句话总结:
+
+
+从这道题目学到了什么，哪些地方需要提升? :
+
+  第二高的薪水
+编写一个 SQL 查询，获取 Employee 表中第二高的薪水（Salary） 。
+
++----+--------+
+| Id | Salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+例如上述 Employee 表，SQL查询应该返回 200 作为第二高的薪水。如果不存在第二高的薪水，那么查询应返回 null。
+
++---------------------+
+| SecondHighestSalary |
++---------------------+
+| 200                 |
++---------------------+
+
+
+
+SELECT
+    IFNULL(
+      (SELECT DISTINCT Salary
+       FROM Employee
+       ORDER BY Salary DESC
+        LIMIT 1 OFFSET 1),
+    NULL) AS SecondHighestSalary
+
+
+
+ * 
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
