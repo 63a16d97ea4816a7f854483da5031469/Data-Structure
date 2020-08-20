8369069038bda436d00ea7c689fda058687fc460
63a16d97ea4816a7f854483da5031469 <ntutonycao@gmail.com>
2020-08-20 21:32:01 +0800

    Updated content.

---
+++ LetOJ/Chi中文Let/剑指 Offer 65. 不用加减乘除做加法.java
@@ -0,0 +1,86 @@
+
+/*
+ * 
+link: 
+https://leetcode-cn.com/problems/bu-yong-jia-jian-cheng-chu-zuo-jia-fa-lcof/
+
+2020-8-20 at 9:28 pm
+
+剑指 Offer 65. 不用加减乘除做加法
+难度
+简单
+
+61
+
+收藏
+
+分享
+切换为英文
+关注
+反馈
+写一个函数，求两个整数之和，要求在函数体内不得使用 “+”、“-”、“*”、“/” 四则运算符号。
+
+ 
+
+示例:
+
+输入: a = 1, b = 1
+输出: 2
+ 
+
+提示：
+
+a, b 均可能是负数或 0
+结果不会溢出 32 位整数
+
+
+对题目易错地方进行总结:
+
+
+对题目的实现思路进行几句话总结:
+
+
+从这道题目学到了什么，哪些地方需要提升? :
+
+
+
+ * 
+ */
+
+
+
+
+
+class Solution {
+    public int add(int a, int b) {
+        while(b != 0) { // 当进位为 0 时跳出
+            int c = (a & b) << 1;  // c = 进位
+            a ^= b; // a = 非进位和
+            b = c; // b = 进位
+        }
+        return a;
+    }
+}
+
+// 作者：jyd
+// 链接：https://leetcode-cn.com/problems/bu-yong-jia-jian-cheng-chu-zuo-jia-fa-lcof/solution/mian-shi-ti-65-bu-yong-jia-jian-cheng-chu-zuo-ji-7/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
