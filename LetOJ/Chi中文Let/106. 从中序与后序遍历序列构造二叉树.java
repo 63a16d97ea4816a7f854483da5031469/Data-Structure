560c1eeb0d3dbaebc140d1766ff10f6ba3133b24
63a16d97ea4816a7f854483da5031469 <ntutonycao@gmail.com>
2020-08-23 22:06:17 +0800

    Updated content.

--- LetOJ/Chi中文Let/106. 从中序与后序遍历序列构造二叉树.java
+++ LetOJ/Chi中文Let/106. 从中序与后序遍历序列构造二叉树.java
@@ -47,12 +47,12 @@ class Solution {
         if(instart>inend) return null;
 
         TreeNode root=new TreeNode(postorder[head--]);
-        int mid=findValue(inorder,root.val);
+        int mid=findMid(inorder,root.val);
         root.right=helper(inorder,postorder,mid+1,inend);
         root.left=helper(inorder,postorder,instart,mid-1);
         return root;
     }
-    public int findValue(int[] inorder, int target){
+    public int findMid(int[] inorder, int target){
         for(int i=0;i<inorder.length;i++){
             if(target==inorder[i]) return i;
         }
@@ -62,7 +62,43 @@ class Solution {
 
 
 
+class Solution {
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        if(inorder==null || postorder==null) {
+            return null;
+        }
+        return helper(inorder,postorder);
+    }
 
+    private TreeNode helper(int[] in, int[] post) {
+        if(in.length==0) {
+            return null;
+        }
+        //根据后序数组的最后一个元素，创建根节点
+        TreeNode root = new TreeNode(post[post.length-1]);
+        //在中序数组中查找值等于【后序数组最后一个元素】的下标
+        for(int i=0;i<in.length;++i) {
+            if(in[i]==post[post.length-1]) {
+                //确定这个下标i后，将中序数组分成两部分，后序数组分成两部分
+                int[] inLeft = Arrays.copyOfRange(in,0,i);
+                int[] inRight = Arrays.copyOfRange(in,i+1,in.length);
+                int[] postLeft = Arrays.copyOfRange(post,0,i);
+                int[] postRight = Arrays.copyOfRange(post,i,post.length-1);
+                //递归处理中序数组左边，后序数组左边
+                root.left = helper(inLeft,postLeft);
+                //递归处理中序数组右边，后序数组右边
+                root.right = helper(inRight,postRight);
+                break;
+            }
+        }
+        return root;
+    }
+}
+
+// 作者：wang_ni_ma
+// 链接：https://leetcode-cn.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/solution/liang-chong-shi-xian-dong-hua-yan-shi-106-cong-zho/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 
 
 
