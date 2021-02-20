f05bfd62313fbc3b5f09d3b7e52d84d795f9447e
63a16d97ea4816a7f854483da5031469 <ntutonycao@gmail.com>
2021-02-20 15:25:48 +0800

    Updated content.

---
+++ LetOJ/Chi中文Let/剑指 Offer 47. 礼物的最大价值.java
@@ -0,0 +1,80 @@
+
+/*
+ * 
+link: 
+https://leetcode-cn.com/problems/shu-zu-zhong-shu-zi-chu-xian-de-ci-shu-lcof/
+
+2021-2-20 at 3:23 pm
+
+剑指 Offer 47. 礼物的最大价值
+难度
+中等
+
+106
+
+在一个 m*n 的棋盘的每一格都放有一个礼物，每个礼物都有一定的价值（价值大于 0）。你可以从棋盘的左上角开始拿格子里的礼物，并每次向右或者向下移动一格、直到到达棋盘的右下角。给定一个棋盘及其上面的礼物的价值，请计算你最多能拿到多少价值的礼物？
+
+示例 1:
+
+输入: 
+[
+  [1,3,1],
+  [1,5,1],
+  [4,2,1]
+]
+输出: 12
+解释: 路径 1→3→5→2→1 可以拿到最多价值的礼物
+ 
+
+提示：
+
+0 < grid.length <= 200
+0 < grid[0].length <= 200
+
+对题目易错地方进行总结:
+
+
+对题目的实现思路进行几句话总结:
+
+
+从这道题目学到了什么，哪些地方需要提升? :
+
+ * 
+ */
+
+
+
+
+class Solution {
+    public int maxValue(int[][] grid) {
+        int row = grid.length;
+        int column = grid[0].length;
+        //dp[i][j]表示从grid[0][0]到grid[i - 1][j - 1]时的最大价值
+        int[][] dp = new int[row + 1][column + 1];
+        for (int i = 1; i <= row; i++) {
+            for (int j = 1; j <= column; j++) {
+                dp[i][j] = Math.max(dp[i - 1][j], dp[i][j - 1]) + grid[i - 1][j - 1];
+            }
+        }
+        return dp[row][column];
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
