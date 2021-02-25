c6fa2add86a3b6da4e8db1b79d567eb74e2a7d19
63a16d97ea4816a7f854483da5031469 <ntutonycao@gmail.com>
2021-02-25 13:55:49 +0800

    Updated content.

--- LetOJ/153. Find Minimum in Rotated Sorted Array.java
+++ LetOJ/153. Find Minimum in Rotated Sorted Array.java
@@ -66,6 +66,28 @@ nums is sorted and rotated between 1 and n times.
  */
 
 
+class Solution {
+    public int findMin(int[] nums) {
+        int l = 0, r = nums.length - 1;
+        // 如果不排除这种情况，[1,2]就会出错。
+        if (nums[r] > nums[l]) return nums[l];
+        while (l < r) {
+            int mid = l + ((r - l) >> 1);
+            // 左边有序，排除左边
+            if (nums[0] <= nums[mid]) {
+                l = mid + 1;
+            } else {
+                r = mid;
+            } 
+        }
+        return nums[l];
+    }
+}
+
+// 作者：bingo-77
+// 链接：https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array/solution/java-er-fen-cha-zhao-by-bingo-77/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 
 
 
@@ -77,7 +99,6 @@ class Solution {
       if (nums.length == 1) {
         return nums[0];
       }
-  
       // initializing left and right pointers.
       int left = 0, right = nums.length - 1;
   
@@ -87,31 +108,25 @@ class Solution {
       if (nums[right] > nums[0]) {
         return nums[0];
       }
-      
-      // Binary search way
       while (right >= left) {
         // Find the mid element
         int mid = left + (right - left) / 2;
-  
         // if the mid element is greater than its next element then mid+1 element is the smallest
         // This point would be the point of change. From higher to lower value.
         if (nums[mid] > nums[mid + 1]) {
           return nums[mid + 1];
         }
-  
         // if the mid element is lesser than its previous element then mid element is the smallest
         if (nums[mid - 1] > nums[mid]) {
           return nums[mid];
         }
-  
         // if the mid elements value is greater than the 0th element this means
         // the least value is still somewhere to the right as we are still dealing with elements
         // greater than nums[0]
         if (nums[mid] > nums[0]) {
           left = mid + 1;
         } else {
-          // if nums[0] is greater than the mid value then this means the smallest value is somewhere to
-          // the left
+          // if nums[0] is greater than the mid value then this means the smallest value is somewhere to the left
           right = mid - 1;
         }
       }
@@ -127,10 +142,29 @@ class Solution {
 
 
 
+public int findMin(int[] nums) {
+    //指针刚开始在第一个元素
+    int needle = 0;
+    //判断是否进行了旋转以及数组是否只有一个元素
+    if(nums[nums.length-1] >= nums[0]){
+        //未旋转或者只有一个元素那么直接返回第一个元素
+        return nums[0];
+    }else {
+        //找到当前元素比下一个元素大的位置
+        while (needle < nums.length - 1 && nums[needle] < nums[needle + 1]) {
+            needle += 1;
+        }
+        return nums[needle+1];
+    }
+}
+
+// 作者：danger-6
+// 链接：https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array/solution/javayong-shi-ji-bai-100-by-danger-6-7j3r/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 
 
-
-
+https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array/solution/yi-wen-dai-ni-gao-ding-er-fen-cha-zhao-j-00kj/
 
 
 
