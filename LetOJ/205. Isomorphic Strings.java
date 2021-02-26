574353f844ac4bf8601237948555c4b2fd19f03b
63a16d97ea4816a7f854483da5031469 <ntutonycao@gmail.com>
2021-02-26 23:15:50 +0800

    Updated content.

---
+++ LetOJ/205. Isomorphic Strings.java
@@ -0,0 +1,82 @@
+
+/*
+ * 
+https://leetcode.com/problems/isomorphic-strings/description/
+
+205. Isomorphic Strings
+Easy
+
+1862
+
+449
+
+Add to List
+
+Share
+Given two strings s and t, determine if they are isomorphic.
+
+Two strings s and t are isomorphic if the characters in s can be replaced to get t.
+
+All occurrences of a character must be replaced with another character while preserving the order of characters. No two characters may map to the same character, but a character may map to itself.
+
+
+Example 1:
+
+Input: s = "egg", t = "add"
+Output: true
+Example 2:
+
+Input: s = "foo", t = "bar"
+Output: false
+Example 3:
+
+Input: s = "paper", t = "title"
+Output: true
+ 
+
+Constraints:
+
+1 <= s.length <= 5 * 104
+t.length == s.length
+s and t consist of any valid ascii character.
+
+26 Feb 2021 at 11:15 pm
+
+
+对题目易错地方进行总结:
+
+
+对题目的实现思路进行几句话总结:
+
+
+从这道题目学到了什么，哪些地方需要提升? :
+
+
+
+
+ * 
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
