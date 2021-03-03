abc76b391cebad30fb15f7272ae3a85b8142986f
63a16d97ea4816a7f854483da5031469 <ntutonycao@gmail.com>
2021-03-03 20:37:15 +0800

    Updated content.

--- LetOJ/Number of Islands II.java
+++ LetOJ/Number of Islands II.java
@@ -3,6 +3,25 @@
  * 
 https://evanyang.gitbooks.io/leetcode/content/LeetCode/number_of_islands_ii.html
 
+https://www.lintcode.com/problem/number-of-islands-ii/
+
+
+https://www.geeksforgeeks.org/union-find/
+
+
+并查集
+
+相关讨论
+相关文章
+
+在计算机科学中，并查集是一种树型的数据结构，用于处理一些不交集（Disjoint Sets）的合并及查询问题。有一个联合-查找算法（Union-find Algorithm）定义了两个用于此数据结构的操作：
+Find：确定元素属于哪一个子集。它可以被用来确定两个元素是否属于同一子集。
+Union：将两个子集合并成同一个集合。
+由于支持这两种操作，一个不相交集也常被称为联合-查找数据结构（Union-find Data Structure）或合并-查找集合（Merge-find Set）。
+为了更加精确的定义这些方法，需要定义如何表示集合。一种常用的策略是为每个集合选定一个固定的元素，称为代表，以表示整个集合。
+https://leetcode-cn.com/tag/union-find/
+
+
 Number of Islands II
 
 A 2d grid map of m rows and n columns is initially filled with water. We may perform an addLand operation which turns the water at position (row, col) into a land. Given a list of positions to operate, count the number of islands after each addLand operation. An island is surrounded by water and is formed by connecting adjacent lands horizontally or vertically. You may assume all four edges of the grid are all surrounded by water.
@@ -10,6 +29,11 @@ A 2d grid map of m rows and n columns is initially filled with water. We may per
 Example:
 
 Given m = 3, n = 3, positions = [[0,0], [0,1], [1,2], [2,1]]. Initially, the 2d grid grid is filled with water. (Assume 0 represents water and 1 represents land).
+
+0 0 0
+0 0 0
+0 0 0
+
 
 0 0 0 
 0 0 0
