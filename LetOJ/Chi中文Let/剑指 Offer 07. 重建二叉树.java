e0be79eec7b60d651a647635dc06e8cb8289986c
63a16d97ea4816a7f854483da5031469 <ntutonycao@gmail.com>
2021-04-03 20:15:03 +0800

    Updated content.

--- LetOJ/Chi中文Let/剑指 Offer 07. 重建二叉树.java
+++ LetOJ/Chi中文Let/剑指 Offer 07. 重建二叉树.java
@@ -97,8 +97,37 @@ class Solution {
 
 
 
+// 使用HashMap映射，快速加速寻找速度
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    int point=0;
+    HashMap<Integer, Integer> indexMap=new HashMap<Integer,Integer>();
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if(preorder==null||preorder.length==0) return null;
+        indexMap = new HashMap<Integer, Integer>();
+        for (int i = 0; i < preorder.length; i++) {
+            indexMap.put(inorder[i], i);
+        }
+        return rebuildTree(preorder,inorder,0,inorder.length-1);
+    }
+    public TreeNode rebuildTree(int[] preorder, int[] inorder,int instart, int inend){
+        if(instart>inend) return null;
+        TreeNode root=new TreeNode(preorder[point++]);
+        int mid=indexMap.get(root.val)==null?-1:indexMap.get(root.val);
+        root.left=rebuildTree(preorder,inorder,instart,mid-1);
+        root.right=rebuildTree(preorder,inorder,mid+1,inend);
 
-
+        return root;
+    }
+}
 
 
 
