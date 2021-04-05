aea87d9a71273e6d91b6f9c55268dc175e59bcd6
63a16d97ea4816a7f854483da5031469 <ntutonycao@gmail.com>
2021-04-05 22:31:46 +0800

    Updated content.

--- LetOJ/Chi中文Let/Unfinished/剑指 Offer 46. 把数字翻译成字符串.java
+++ LetOJ/Chi中文Let/Unfinished/剑指 Offer 46. 把数字翻译成字符串.java
@@ -133,6 +133,59 @@ class Solution {
 
 
 
+class Solution {
+    public int translateNum(int num) {
+        String src = String.valueOf(num);
+        int p = 0, q = 0, r = 1;
+        for (int i = 0; i < src.length(); ++i) {
+            p = q; 
+            q = r; 
+            r = 0;
+            r += q;
+            if (i == 0) {
+                continue;
+            }
+            String pre = src.substring(i - 1, i + 1);
+            if (pre.compareTo("25") <= 0 && pre.compareTo("10") >= 0) {
+                r += p;
+            }
+        }
+        return r;
+    }
+}
+
+// 作者：LeetCode-Solution
+// 链接：https://leetcode-cn.com/problems/ba-shu-zi-fan-yi-cheng-zi-fu-chuan-lcof/solution/ba-shu-zi-fan-yi-cheng-zi-fu-chuan-by-leetcode-sol/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+class Solution{
+    public int translateNum(int num) {
+        //将字符串转化为数字
+        String str = String.valueOf(num);
+        //dfs遍历字符串求解
+        return dfs(str, 0);
+    }
+    //表示从index位置开始有好多种翻译方法
+    public int dfs(String str, int index){
+        //如果当前的下标大于等于字符串的长度 - 1，则说明当前位置是由上一次跳到此处的，则直接返回1
+        if(index >= str.length() - 1)
+            return 1;
+        //先求解每一次都翻译一个字符的方法数
+        int res = dfs(str, index + 1);
+        //以当前字符的下标为开始，截取两位，判断这位组成的数字是否在10~25之间
+        //如果在这一次就可以直接翻译两个字符，然后从两个字符后面的位置开始翻译
+        String temp = str.substring(index, index + 2);
+        if(temp.compareTo("10") >= 0 && temp.compareTo("25") <= 0)
+            res += dfs(str, index + 2);
+        return res;
+    }
+}
+
+
+
+
+
 public class Solution {
 
     public int translateNum(int num) {
