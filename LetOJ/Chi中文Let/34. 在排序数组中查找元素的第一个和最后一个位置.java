853af3dfdae9f4a4356212d93b6ae95f9b1ea139
63a16d97ea4816a7f854483da5031469 <ntutonycao@gmail.com>
2022-01-24 16:33:24 +0800

    Updated content.

--- LetOJ/Chi中文Let/34. 在排序数组中查找元素的第一个和最后一个位置.java
+++ LetOJ/Chi中文Let/34. 在排序数组中查找元素的第一个和最后一个位置.java
@@ -102,11 +102,49 @@ class Solution {
 
 
 
+方法一：二分查找
+
+由于数组已经排序，因此整个数组是单调递增的，我们可以利用二分法来加速查找的过程。
+二分查找中，寻找 leftIdx 即为在数组中寻找第一个大于等于 target 的下标，寻找 rightIdx 即为在数组中寻找第一个大于 target 的下标，然后将下标减一。
+
+作者：LeetCode-Solution
+链接：https://leetcode-cn.com/problems/find-first-and-last-position-of-element-in-sorted-array/solution/zai-pai-xu-shu-zu-zhong-cha-zhao-yuan-su-de-di-3-4/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 
 
 
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        int leftIdx = binarySearch(nums, target, true);
+        int rightIdx = binarySearch(nums, target, false) - 1;
+        if (leftIdx <= rightIdx && rightIdx < nums.length && nums[leftIdx] == target && nums[rightIdx] == target) {
+            return new int[]{leftIdx, rightIdx};
+        } 
+        return new int[]{-1, -1};
+    }
 
+    public int binarySearch(int[] nums, int target, boolean lower) {
+        int left = 0, right = nums.length - 1, ans = nums.length;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (nums[mid] > target || (lower && nums[mid] >= target)) {
+                right = mid - 1;
+                ans = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
 
+O(logn)
+
+// 作者：LeetCode-Solution
+// 链接：https://leetcode-cn.com/problems/find-first-and-last-position-of-element-in-sorted-array/solution/zai-pai-xu-shu-zu-zhong-cha-zhao-yuan-su-de-di-3-4/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 
 
 
---
+++ LetOJ/Chi中文Let/70. 爬楼梯.java
@@ -0,0 +1,113 @@
+
+/*
+ * 
+link: https://leetcode-cn.com/problems/climbing-stairs/
+
+
+70. 爬楼梯
+难度
+简单
+
+2136
+
+收藏
+
+分享
+切换为英文
+接收动态
+反馈
+假设你正在爬楼梯。需要 n 阶你才能到达楼顶。
+
+每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？
+
+ 
+
+示例 1：
+
+输入：n = 2
+输出：2
+解释：有两种方法可以爬到楼顶。
+1. 1 阶 + 1 阶
+2. 2 阶
+示例 2：
+
+输入：n = 3
+输出：3
+解释：有三种方法可以爬到楼顶。
+1. 1 阶 + 1 阶 + 1 阶
+2. 1 阶 + 2 阶
+3. 2 阶 + 1 阶
+ 
+
+提示：
+
+1 <= n <= 45
+
+
+2022-01-23 at 22:35
+ 
+
+对题目易错地方进行总结:
+
+
+对题目的实现思路进行几句话总结:
+
+
+从这道题目学到了什么，哪些地方需要提升? :
+
+
+
+ * 
+ */
+
+
+超时：
+
+class Solution {
+    public int climbStairs(int n) {
+       return f(n);
+    }
+    public int f(int n){
+        if(n==0) return 0;
+        if(n==1) return 1;
+        if(n==2) return 2;
+        return f(n-2)+f(n-1);
+    }
+}
+
+
+
+
+通过：
+class Solution {
+    public int climbStairs(int n) {
+       return f(n);
+    }
+    public int f(int n){
+        if(n==1) return 1;
+        if(n==2) return 2;
+        int a=1;
+        int b=2;
+        for(int i=2;i<n;i++){
+            int tmp=b;
+            b=a+b;
+            a=tmp;
+        }
+        return b;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
