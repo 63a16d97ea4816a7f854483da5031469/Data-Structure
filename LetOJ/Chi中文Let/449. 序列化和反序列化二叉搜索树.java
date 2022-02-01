e1bb581036c14594966310c9be751be94ac9694e
63a16d97ea4816a7f854483da5031469 <ntutonycao@gmail.com>
2022-02-01 14:41:50 +0800

    Updated content.

--- LetOJ/Chi中文Let/449. 序列化和反序列化二叉搜索树.java
+++ LetOJ/Chi中文Let/449. 序列化和反序列化二叉搜索树.java
@@ -244,10 +244,10 @@ class Codec {
     }
 }
 
-作者：sdwwld
-链接：https://leetcode-cn.com/problems/serialize-and-deserialize-bst/solution/bfsjie-jue-he-dfsjie-jue-by-sdwwld-mgxp/
-来源：力扣（LeetCode）
-著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+// 作者：sdwwld
+// 链接：https://leetcode-cn.com/problems/serialize-and-deserialize-bst/solution/bfsjie-jue-he-dfsjie-jue-by-sdwwld-mgxp/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 
 
 
