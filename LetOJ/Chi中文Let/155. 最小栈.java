75c83cdd84d5755bf1dfde99f164864da8d75d89
63a16d97ea4816a7f854483da5031469 <ntutonycao@gmail.com>
2022-02-03 23:41:38 +0800

    Updated content.

--- LetOJ/Chi中文Let/155. 最小栈.java
+++ LetOJ/Chi中文Let/155. 最小栈.java
@@ -75,7 +75,16 @@ pop、top 和 getMin 操作总是在 非空栈 上调用。
  * 
  */
 
+数据栈+最小栈
+这道题标准的解法是用两个栈，一个存放数据，一个存放当前最小值，空间换时间。
+把两个栈封装在一个栈中，其中的一个栈存放正常的元素，另一个栈 minStack 只存当前最小值。
+push：数据栈正常操作。比较当前元素和最小栈minStack的栈顶，比栈顶还小则进栈，否则push原栈顶。
+pop：数据栈正常操作。最小栈minStack也pop
+getMin：直接返回最小栈栈顶
 
+push，pop，top，getMin 时间复杂度都是 O(1)，空间复杂度 O(n)
+
+这里还可以有个优化，最小栈并不和数据栈同步更新，只是每次push遇到小于等于栈顶的才放入。pop时如果出栈的数据就是最小栈栈顶最小栈，则最小栈也pop。
 
 class MinStack {
     Stack<Integer> stack;
@@ -117,17 +126,119 @@ class MinStack {
 
 
 
+http://masikkk.com/article/LeetCode.155.MinStack/
+
+栈+最小堆(优先队列)
+没想到很好的思路，用 栈 + 优先队列（最小堆） 实现的。
+
+push 优先队列offer O(logn)
+pop 优先队列remove(x) O(n)
+top O(1)
+getMin O(1)
+空间复杂度 O(n)
 
 
+private static class MinStack {
+    private Deque<Integer> stack;
+    private PriorityQueue<Integer> priorityQueue;
+    /** initialize your data structure here. */
+    public MinStack() {
+        stack = new LinkedList<>();
+        // 优先队列，小顶堆，堆顶永远是最小值
+        priorityQueue = new PriorityQueue<>();
+    }
+    public void push(int x) {
+        stack.push(x);
+        priorityQueue.offer(x);
+    }
+    public void pop() {
+        Integer i = stack.pop();
+        // remove(x) 的时间复杂度为O(n)
+        priorityQueue.remove(i);
+    }
+    public int top() {
+        return stack.peek();
+    }
+    public int getMin() {
+        return priorityQueue.peek();
+    }
+}
 
 
+https://github.com/azl397985856/leetcode/blob/master/problems/155.min-stack.md
+
+关键点
+
+最小栈存储的不应该是真实值，而是真实值和min的差值
+top的时候涉及到对数据的还原，这里千万注意是上一个最小值
 
 
+是否有更高效的算法呢？答案是有的。
+
+我们每次入栈的时候，保存的不再是真正的数字，而是它与当前最小值的差（当前元素没有入栈的时候的最小值）。 这样我们pop和top的时候拿到栈顶元素再加上上一个最小值即可。 另外我们在push和pop的时候去更新min，这样getMin的时候就简单了，直接返回min。
+
+注意上面加粗的“上一个”，不是“当前的最小值”
+经过上面的分析，问题的关键转化为“如何求得上一个最小值”，解决这个的关键点在于利用min。
+
+pop或者top的时候：
+
+如果栈顶元素小于0，说明栈顶是当前最小的元素，它出栈会对min造成影响，我们需要去更新min。 上一个最小的是“min - 栈顶元素”,我们需要将上一个最小值更新为当前的最小值
+因为栈顶元素入栈的时候的通过 栈顶元素 = 真实值 - 上一个最小的元素 得到的， 而真实值 = min， 因此可以得出上一个最小的元素 = 真实值 -栈顶元素
+如果栈顶元素大于0，说明它对最小值没有影响，上一个最小值就是上上个最小值。
 
 
-
-
-
+class MinStack {
+    long min;
+    Stack<Long> stack;
+    
+    /** initialize your data structure here. */
+    public MinStack() {
+        stack = new Stack<>();
+    }
+    
+    public void push(int x) {
+        if (stack.isEmpty()) {
+            stack.push(0L);
+            min = x;
+        }
+        else {
+            stack.push(x - min);
+            if (x < min)
+                min = x;
+        }
+    }
+    
+    public void pop() {
+        long p = stack.pop();
+        
+        if (p < 0) {
+            // if (p < 0), the popped value is the min
+            // Recall p is added by this statement: stack.push(x - min);
+            // So, p = x - old_min
+            // old_min = x - p
+            // again, if (p < 0), x is the min so:
+            // old_min = min - p
+            min = min - p;
+        }
+    }
+    
+    public int top() {
+        long p = stack.peek();
+        
+        if (p < 0) {
+            return (int) min;
+        }
+        else {
+            // p = x - min
+            // x = p + min
+            return (int) (p + min);
+        }
+    }
+    
+    public int getMin() {
+        return (int) min;    
+    }
+}
 
 
 
