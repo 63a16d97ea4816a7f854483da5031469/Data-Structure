f6f068a339f4ff55b07b92e0b32321a90cf223a3
63a16d97ea4816a7f854483da5031469 <ntutonycao@gmail.com>
2022-02-07 00:16:27 +0800

    Updated content.

---
+++ LetOJ/Chi中文Let/106. 从中序与后序遍历序列构造二叉树-2.java
@@ -0,0 +1,136 @@
+
+/*
+ * 
+link: 
+https://leetcode-cn.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/
+
+2022-02-06 at 23:28
+
+
+对题目易错地方进行总结:
+
+
+对题目的实现思路进行几句话总结:
+
+
+从这道题目学到了什么，哪些地方需要提升? :
+
+
+
+ * 
+ */
+
+
+
+
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    //9.01pm-9.09
+    int head=0;
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        head=postorder.length-1;
+        return helper(inorder,postorder,0,inorder.length-1);
+    }
+
+    public TreeNode helper(int[]inorder,int[] postorder,int instart,int inend){
+        if(instart>inend) return null;
+
+        TreeNode root=new TreeNode(postorder[head--]);
+        int mid=findMid(inorder,root.val);
+        root.right=helper(inorder,postorder,mid+1,inend);
+        root.left=helper(inorder,postorder,instart,mid-1);
+        return root;
+    }
+    public int findMid(int[] inorder, int target){
+        for(int i=0;i<inorder.length;i++){
+            if(target==inorder[i]) return i;
+        }
+        return 0;
+    }
+}
+
+
+
+class Solution {
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        if(inorder==null || postorder==null) {
+            return null;
+        }
+        return helper(inorder,postorder);
+    }
+
+    private TreeNode helper(int[] in, int[] post) {
+        if(in.length==0) {
+            return null;
+        }
+        //根据后序数组的最后一个元素，创建根节点
+        TreeNode root = new TreeNode(post[post.length-1]);
+        //在中序数组中查找值等于【后序数组最后一个元素】的下标
+        for(int i=0;i<in.length;++i) {
+            if(in[i]==post[post.length-1]) {
+                //确定这个下标i后，将中序数组分成两部分，后序数组分成两部分
+                int[] inLeft = Arrays.copyOfRange(in,0,i);
+                int[] inRight = Arrays.copyOfRange(in,i+1,in.length);
+                int[] postLeft = Arrays.copyOfRange(post,0,i);
+                int[] postRight = Arrays.copyOfRange(post,i,post.length-1);
+                //递归处理中序数组左边，后序数组左边
+                root.left = helper(inLeft,postLeft);
+                //递归处理中序数组右边，后序数组右边
+                root.right = helper(inRight,postRight);
+                break;
+            }
+        }
+        return root;
+    }
+}
+
+// 作者：wang_ni_ma
+// 链接：https://leetcode-cn.com/problems/construct-binary-tree-from-inorder-and-postorder-traversal/solution/liang-chong-shi-xian-dong-hua-yan-shi-106-cong-zho/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+
+
+class Solution {
+    int index=0;
+    public TreeNode buildTree(int[] inorder, int[] postorder) {
+        index=postorder.length-1;
+        return build(inorder,postorder,0,postorder.length-1);
+    }
+
+    public TreeNode build(int[] inorder, int[] postorder, int start, int end){
+        if(index<0) return null;
+        if(start>end) return null;
+
+        TreeNode root=new TreeNode(postorder[index--]);
+        int point=findIndex(inorder,root.val);
+        //注意要 Right在前，因为postorder遍历的顺序是这样的:
+        root.right=build(inorder,postorder,point+1,end);
+        root.left=build(inorder,postorder,start,point-1);
+        return root;
+    }
+
+    public int findIndex(int[] inorder, int target){
+        for(int i=0;i<inorder.length;i++){
+            if(inorder[i]==target){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
+
+
+
+
---
+++ LetOJ/Chi中文Let/889. 根据前序和后序遍历构造二叉树-2.java
@@ -0,0 +1,111 @@
+
+/*
+ * 
+link: 
+
+
+2020-7-1 at 8:33 pm
+
+
+对题目易错地方进行总结:
+
+
+对题目的实现思路进行几句话总结:
+
+
+从这道题目学到了什么，哪些地方需要提升? :
+
+
+
+ * 
+ */
+
+
+
+
+
+class Solution {
+    public TreeNode constructFromPrePost(int[] pre, int[] post) {
+        if(pre==null || pre.length==0) {
+            return null;
+        }
+        return dfs(pre,post);
+    }
+	
+    private TreeNode dfs(int[] pre,int[] post) {
+        if(pre==null || pre.length==0) {
+            return null;
+        }
+        //数组长度为1时，直接返回即可
+        if(pre.length==1) {
+            return new TreeNode(pre[0]);
+        }
+        //根据前序数组的第一个元素，创建根节点 
+        TreeNode root = new TreeNode(pre[0]);
+        int n = pre.length;
+        for(int i=0;i<post.length;++i) {
+            if(pre[1]==post[i]) {
+                //根据前序数组第二个元素，确定后序数组左子树范围
+                int left_count = i+1;
+                //拆分前序和后序数组，分成四份
+                int[] pre_left = Arrays.copyOfRange(pre,1,left_count+1);
+                int[] pre_right = Arrays.copyOfRange(pre,left_count+1,n);
+                int[] post_left = Arrays.copyOfRange(post,0,left_count);
+                int[] post_right = Arrays.copyOfRange(post,left_count,n-1);
+                //递归执行前序数组左边、后序数组左边
+                root.left = dfs(pre_left,post_left);
+                //递归执行前序数组右边、后序数组右边
+                root.right = dfs(pre_right,post_right);
+                break;
+            }
+        }
+        //返回根节点
+        return root;
+    }
+}	
+
+// 作者：wang_ni_ma
+// 链接：https://leetcode-cn.com/problems/construct-binary-tree-from-preorder-and-postorder-traversal/solution/tu-jie-889-gen-ju-qian-xu-he-hou-xu-bian-li-gou-2/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+
+
+
+
+
+class Solution {
+    public TreeNode constructFromPrePost(int[] pre, int[] post) {
+        int N = pre.length;
+        if (N == 0) return null;
+        TreeNode root = new TreeNode(pre[0]);
+        if (N == 1) return root;
+
+        int L = 0;
+        for (int i = 0; i < N; ++i)
+            if (post[i] == pre[1])
+                L = i+1;
+
+        root.left = constructFromPrePost(Arrays.copyOfRange(pre, 1, L+1),
+                                         Arrays.copyOfRange(post, 0, L));
+        root.right = constructFromPrePost(Arrays.copyOfRange(pre, L+1, N),
+                                          Arrays.copyOfRange(post, L, N-1));
+        return root;
+    }
+}
+
+// 作者：LeetCode
+// 链接：https://leetcode-cn.com/problems/construct-binary-tree-from-preorder-and-postorder-traversal/solution/gen-ju-qian-xu-he-hou-xu-bian-li-gou-zao-er-cha-sh/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+
+
+
+
+
+
+
+
+
+
+
