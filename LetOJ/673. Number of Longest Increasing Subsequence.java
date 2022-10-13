835ab85cd232cd718922e3d989ae4d175bdcd401
63a16d97ea4816a7f854483da5031469 <ntutonycao@gmail.com>
2022-10-13 23:09:09 +0800

    Updated content.

---
+++ LetOJ/673. Number of Longest Increasing Subsequence.java
@@ -0,0 +1,76 @@
+
+/*
+ * 
+https://leetcode.com/problems/number-of-longest-increasing-subsequence/
+
+673. Number of Longest Increasing Subsequence
+Medium
+
+4287
+
+194
+
+Add to List
+
+Share
+Given an integer array nums, return the number of longest increasing subsequences.
+
+Notice that the sequence has to be strictly increasing.
+
+ 
+
+Example 1:
+
+Input: nums = [1,3,5,4,7]
+Output: 2
+Explanation: The two longest increasing subsequences are [1, 3, 4, 7] and [1, 3, 5, 7].
+Example 2:
+
+Input: nums = [2,2,2,2,2]
+Output: 5
+Explanation: The length of the longest increasing subsequence is 1, and there are 5 increasing subsequences of length 1, so output 5.
+ 
+
+Constraints:
+
+1 <= nums.length <= 2000
+-106 <= nums[i] <= 106
+
+
+对题目易错地方进行总结:
+
+
+对题目的实现思路进行几句话总结:
+
+
+从这道题目学到了什么，哪些地方需要提升? :
+
+
+
+
+ * 
+ */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
