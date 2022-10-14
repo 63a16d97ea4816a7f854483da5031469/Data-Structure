90c4da84ca9b7f6b7d5fd618b33d4d7a1a46aa14
63a16d97ea4816a7f854483da5031469 <ntutonycao@gmail.com>
2022-10-14 23:47:51 +0800

    Updated content.

--- LetOJ/Chi中文Let/63. 不同路径 II.java
+++ LetOJ/Chi中文Let/63. 不同路径 II.java
@@ -138,6 +138,61 @@ class Solution {
 
 
 
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        int m=obstacleGrid.length;
+        int n=obstacleGrid[0].length;
+        
+        //极限条件
+        if(obstacleGrid[0][0]==1) return 0;
+        
+        int[][] dp=new int[m][n];
+        dp[0][0]=0;
+        
+        // for(int i=0;i<m;i++){
+        //     dp[i][0]=1;
+        // }
+        // for(int j=0;j<n;j++){
+        //     dp[0][j]=1;
+        // }
+        
+        for(int i=0;i<m;i++){
+            if(obstacleGrid[i][0]==0){
+                dp[i][0]=1;
+            }else{
+                break;//如果道路阻挡，就要对后面的不再赋值，因为后面不可达
+            }
+        }
+        for(int j=0;j<n;j++){
+            if(obstacleGrid[0][j]==0){
+                dp[0][j]=1;
+            }else{
+                break; //如果道路阻挡，就要对后面的不再赋值，因为后面不可达
+            }
+        }
+        
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(obstacleGrid[i][j]==1){
+                    dp[i][j]=0;  //对矩阵边界进行修正
+                }
+            }
+        }
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                if(obstacleGrid[i][j]==0){ // 对是否有障碍物，进行验证
+                    dp[i][j]=dp[i-1][j]+dp[i][j-1];
+                }else{
+                    dp[i][j]=0;
+                }
+            }
+        }
+        
+        return dp[m-1][n-1];
+    }
+}
+
+
 
 
 
